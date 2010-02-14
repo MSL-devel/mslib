680e4d7a9a94c8c8ebf18b88aaa17248d3162546
Alessandro Senes <senes@wisc.edu>
2010-02-14 23:51:19 +0000

    Missing test for the SASA calculator that was not added previously

---
+++ tests/testSasaCalculator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#include "System.h"
+#include "SasaCalculator.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+	// the program requires the location of the "exampleFiles" as an argument
+	if (argc < 2) {
+		cerr << "USAGE:\ntestSasaCalculator <file.pdb>" << endl;
+		exit(0);
+	}
+
+	System sys;
+	if (!sys.readPdb(argv[1])) {
+		cerr << "Cannot read pdb " << argv[0] << endl;
+	}
+
+	SasaCalculator sas(sys.getAtoms());
+	sas.calcSasa();
+	sas.printSasaTable();
+	cout << endl;
+	sas.printResidueSasaTable();
+
+	
+
+	return 0;
+}
+
