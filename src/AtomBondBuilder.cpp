268111c664881b9241800c64146fa94e11d74781
Alessandro Senes <senes@wisc.edu>
2010-02-14 23:51:08 +0000

    New object: it adds bonded information to the atoms based on their distance

---
+++ src/AtomBondBuilder.cpp
@@ -0,0 +1,82 @@
+#include "AtomBondBuilder.h"
+
+AtomBondBuilder::AtomBondBuilder() {
+	setup();
+}
+
+AtomBondBuilder::AtomBondBuilder(const AtomBondBuilder & _abb) {
+	setup();
+	atomRadii = _abb.atomRadii;
+}
+
+AtomBondBuilder::~AtomBondBuilder() {
+}
+
+void AtomBondBuilder::setup() {
+	useDefaultRadii = true;
+	atomRadii["C"] = 2.00;
+	atomRadii["H"] = 1.09;
+	atomRadii["N"] = 1.85;
+	atomRadii["O"] = 1.735;
+	atomRadii["CA"] = 1.71;
+	atomRadii["FE"] = 0.65;
+	atomRadii["S"] = 2.06;
+	atomRadii["ZN"] = 1.09;
+	atomRadii["HE"] = 1.48;
+	atomRadii["NE"] = 1.53;
+	atomRadii["NA"] = 1.36375;
+	atomRadii["K"] = 1.76375;
+	atomRadii["CL"] = 2.27;
+	atomRadii["MG"] = 1.185;
+	atomRadii["CS"] = 2.1;
+	atomRadii["CU"] = 1.38;
+	atomRadii["P"] = 2.15;
+	atomRadii["SE"] = 2.20;
+	factor = 0.47;
+}
+
+void AtomBondBuilder::buildConnections(AtomPointerVector & _atoms) {
+
+	for (AtomPointerVector::iterator k=_atoms.begin(); k!=_atoms.end(); k++) {
+		for (AtomPointerVector::iterator l=k+1; l!=_atoms.end(); l++) {
+
+			string atomk = (*k)->getElement();
+			string atoml = (*l)->getElement();
+
+			float kRad = 0;
+			float lRad = 0;
+
+			if (atomRadii.find(atomk) != atomRadii.end()) {
+				kRad = atomRadii[atomk];
+			}
+
+			if (atomRadii.find(atomk) != atomRadii.end()) {
+				lRad = atomRadii[atoml];
+			}
+
+			float dist = (*k)->distance(**l);
+			float vdwrSum = kRad + lRad; 
+
+			if (dist < (vdwrSum*factor)) {
+		//		cout << (**k) << " to " << (**l) << " at: " << dist << endl;
+				(*k)->setBoundTo(*l);
+			}
+
+
+		}
+	}
+
+	/*
+	// Future testing: use short PDB file and CHARMM and compare the number of bonds present	
+	for (AtomPointerVector::iterator k=_atoms.begin(); k!=_atoms.end(); k++) {
+		cout << (**k) << endl;
+		vector<Atom*> bondMap = (*k)->getBonds();
+		for (vector<Atom*>::iterator l=bondMap.begin(); l!=bondMap.end(); l++) {
+			cout << "   " << **l << endl;
+		}
+		cout << endl;
+	}
+	*/
+
+
+}
---
+++ src/AtomBondBuilder.h
@@ -0,0 +1,61 @@
+/*
+----------------------------------------------------------------------------
+This file is part of MSL (Molecular Simulation Library)n
+ Copyright (C) 2009 Dan Kulp, Alessandro Senes, Jason Donald, Brett Hannigan,
+ Sabareesh Sabraminiam
+
+This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, 
+ USA, or go to http://www.gnu.org/copyleft/lesser.txt.
+----------------------------------------------------------------------------
+*/
+
+#ifndef ATOMBONDBUILDER_H
+#define ATOMBONDBUILDER_H
+
+#include "AtomPointerVector.h"
+
+using namespace std;
+
+/****************************************************************
+  TODO: implement a way to use custom radii
+ ****************************************************************/
+
+class AtomBondBuilder {
+
+	public:
+		AtomBondBuilder();
+		AtomBondBuilder(const AtomBondBuilder & _abb);
+		~AtomBondBuilder();
+
+		void buildConnections(AtomPointerVector & _atoms);
+
+		void setFactor(double _factor);
+		double getFactor() const;
+
+	private:
+		void setup();
+
+		map <string,double> atomRadii;
+		bool useDefaultRadii;
+
+		double factor; //multiplying factor for vdwrSum
+
+};
+inline void AtomBondBuilder::setFactor(double _factor) {factor = _factor;}
+inline double AtomBondBuilder::getFactor() const {return factor;}
+
+
+#endif
+
