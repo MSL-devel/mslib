09e8473bd5d27bcccf1deb07c9e2896c0ea64ad9
Dan Kulp <dwkulp@gmail.com>
2010-02-16 15:18:36 +0000

    Added generic use of Regular Expressions in MslTools

--- tests/testRegEx.cpp
+++ tests/testRegEx.cpp
@@ -32,4 +32,35 @@ int main(){
 
 
 
+
+	// Generic string matching
+	string regExpression = "^CLUSTAL W (\\S+)\\s+([\\S\\s]+)$";
+        string lineToMatch="CLUSTAL W 2.1 multiple sequence alignment";
+        string lineNotToMatch="LUTAL W 2.1 multiple sequence alignment";
+
+	vector<string> results;
+
+	// Line which should match RegEx
+	if (MslTools::regex(lineToMatch,regExpression,results)){
+
+	  cout << "PASS test 1\n";
+
+	  // Print out the matched '()' substrings
+	  for (uint i = 0; i < results.size();i++){
+	    cout << "Match["<<i<<"]: "<<results[i]<<endl;
+	  }
+
+	} else {
+	  cerr << "ERROR did not match line properly.\n";
+	}
+
+
+	// Line which should NOT match RegEx
+	if (MslTools::regex(lineNotToMatch,regExpression,results)){
+	  cerr << "ERROR matched line when it was not suppose to\n";
+	} else {
+	  cout << "PASS test 2 , did not match a string that it should not match\n";
+	}
+
+
 }
