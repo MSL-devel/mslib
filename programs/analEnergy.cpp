cbb7f8357b5b334e977281c94129874e68b0b45a
Dan Kulp <dwkulp@gmail.com>
2010-03-04 14:54:04 +0000

    Uses a parameter file for EnergeticAnalysis

--- programs/analEnergy.cpp
+++ programs/analEnergy.cpp
@@ -56,6 +56,7 @@ int main(int argc, char *argv[]) {
 
 	outSys.writePdb("/tmp/preEA.pdb");
 	EnergeticAnalysis ea;
+	ea.setParameterFile(opt.parfile);
 
 	for (uint i = 0; i < opt.positions.size();i++){
 
