3d93ebe829c5d639754aa9b947321326b02ec529
Alessandro Senes <senes@wisc.edu>
2010-03-09 17:49:17 +0000

    Fixed after atom/res/pos/chain

--- tests/testResidueSubstitutionTable.cpp
+++ tests/testResidueSubstitutionTable.cpp
@@ -1,7 +1,8 @@
 /*
 ----------------------------------------------------------------------------
-This file is part of MSL (Molecular Simulation Library)n
- Copyright (C) 2009 Dan Kulp, Alessandro Senes, Jason Donald, Brett Hannigan
+This file is part of MSL (Molecular Software Libraries)
+ Copyright (C) 2010 Dan Kulp, Alessandro Senes, Jason Donald, Brett Hannigan,
+ Sabareesh Subramaniam, Ben Mueller
 
 This library is free software; you can redistribute it and/or
  modify it under the terms of the GNU Lesser General Public
@@ -19,6 +20,7 @@ You should have received a copy of the GNU Lesser General Public
  USA, or go to http://www.gnu.org/copyleft/lesser.txt.
 ----------------------------------------------------------------------------
 */
+
 
 #include "ResidueSubstitutionTable.h"
 #include "ResidueSubstitutionTableReader.h"
@@ -61,7 +63,7 @@ int main(int argc, char **argv){
         PDBWriter pdbout("test.pdb");
         pdbout.open();
 
-	for (uint i = 0; i < sys.residueSize();i++){
+	for (uint i = 0; i < sys.positionSize();i++){
             Residue &currResidue = sys.getResidue(i);
             Residue newResidue = rst.replaceResidue(currResidue);
             pdbout.write(newResidue.getAtoms(), false);
