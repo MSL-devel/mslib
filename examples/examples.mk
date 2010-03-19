791b788da10dcd0d11ff649e8e50a6cd0a34f9b5
Alessandro Senes <senes@wisc.edu>
2010-03-19 20:11:35 +0000

    Added example_looping_over_Chain_Residues_Atoms example_AtomPointerVector

--- examples/examples.mk
+++ examples/examples.mk
@@ -1,3 +1,4 @@
 
-EXAMPLES = example_multipleAtomsCoordinates example_multipleResidueIdentities example_SasaCalculator_usage example_read_write_PDBs_with_the_AtomContainer example_read_write_PDBs_with_the_System 
+EXAMPLES = example_multipleAtomsCoordinates example_multipleResidueIdentities example_SasaCalculator_usage example_read_write_PDBs_with_the_AtomContainer \
+           example_read_write_PDBs_with_the_System example_looping_over_Chain_Residues_Atoms example_AtomPointerVector
 
