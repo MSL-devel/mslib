07a0e9737433cd9c5882ef35387b81d952f3a36d
Alessandro Senes <senes@wisc.edu>
2010-04-06 03:09:37 +0000

    Only updated file header

--- src/CharmmTopologyResidue.cpp
+++ src/CharmmTopologyResidue.cpp
@@ -1,7 +1,8 @@
 /*
 ----------------------------------------------------------------------------
 This file is part of MSL (Molecular Simulation Library)n
- Copyright (C) 2009 Dan Kulp, Alessandro Senes, Jason Donald, Brett Hannigan
+ Copyright (C) 2010 Dan Kulp, Alessandro Senes, Jason Donald, Brett Hannigan,
+ Sabareesh Subramaniam, Ben Mueller
 
 This library is free software; you can redistribute it and/or
  modify it under the terms of the GNU Lesser General Public
