0b1c66b1524f1474b69dddfb12fc187ff1693206
Alessandro Senes <senes@wisc.edu>
2010-04-06 03:09:47 +0000

    Added tests for the removal of bonds from atoms

--- tests/testAtomBondBuilder.cpp
+++ tests/testAtomBondBuilder.cpp
@@ -38,13 +38,14 @@ int main(int argc,char *argv[]) {
 	rAv.open(argv[1]);
 	rAv.read();
 	*/
-	if (argc == 1) {
-		cout << "Please specify a PDB file" << endl;
-		exit(0);
-	}
+//	if (argc == 1) {
+//		cout << "Please specify a PDB file" << endl;
+//		exit(0);
+//	}
+	string file = "exampleFiles/example0001.pdb";
 	System sys;
-	if (!sys.readPdb(argv[1])) {
-		cerr << "Cannot read pdb file " << argv[1] << endl;
+	if (!sys.readPdb(file)) {
+		cerr << "Cannot read pdb file " << file << endl;
 		exit(1);
 	}
 	AtomPointerVector av = sys.getAtomPointers();
@@ -52,13 +53,133 @@ int main(int argc,char *argv[]) {
 
 	AtomBondBuilder abb;
 	abb.buildConnections(av);
+	cout << "==========================" << endl;
+	for (unsigned int i=0; i<av.size(); i++) {
+		char c [1000];
+		sprintf(c, "%04d", i);
+		cout << c << " " << *av[i] << "  : ";
+		for (unsigned int j=0; j<av.size(); j++) {
+			if (i == j) {
+				cout << "1";
+			} else if (av[i]->isBoundTo(av[j])) {
+				cout << "2";
+			} else if (av[i]->isOneThree(av[j])) {
+				cout << "3";
+			} else if (av[i]->isOneFour(av[j])) {
+				cout << "4";
+			} else {
+				cout << "-";
+			}
+		}
+		cout << endl;
+	}
+
+	cout << "==========================" << endl;
 
 	for (unsigned int i=0; i<av.size(); i++) {
-		cout << *av[i] << endl;
+		cout << av[i]->getAtomId() << " is bound to" << endl;
 		vector<Atom*> bonded = av[i]->getBonds();
 		for (unsigned int j=0; j<bonded.size(); j++) {
-			cout << "   " << *bonded[j] << endl;
+			cout << "   " << bonded[j]->getAtomId() << endl;
 		}
+	}
+	cout << "==========================" << endl;
+
+	av[6]->setUnboundFrom(av[5]);	// 1N 2CA
+
+	cout << "==========================" << endl;
+	cout << "Let's now manually remove some bonds" << endl;
+	cout << "Removed bond between atoms 1N-2CA" << endl;
+	for (unsigned int i=0; i<av.size(); i++) {
+		char c [1000];
+		sprintf(c, "%04d", i);
+		cout << c << " " << *av[i] << "  : ";
+		for (unsigned int j=0; j<av.size(); j++) {
+			if (i == j) {
+				cout << "1";
+			} else if (av[i]->isBoundTo(av[j])) {
+				cout << "2";
+			} else if (av[i]->isOneThree(av[j])) {
+				cout << "3";
+			} else if (av[i]->isOneFour(av[j])) {
+				cout << "4";
+			} else {
+				cout << "-";
+			}
+		}
+		cout << endl;
+	}
+
+	av[7]->setUnboundFrom(av[6]);	// 2CA 2CB
+
+	cout << "==========================" << endl;
+	cout << "Removed bond between atoms 2CA-2CB" << endl;
+	for (unsigned int i=0; i<av.size(); i++) {
+		char c [1000];
+		sprintf(c, "%04d", i);
+		cout << c << " " << *av[i] << "  : ";
+		for (unsigned int j=0; j<av.size(); j++) {
+			if (i == j) {
+				cout << "1";
+			} else if (av[i]->isBoundTo(av[j])) {
+				cout << "2";
+			} else if (av[i]->isOneThree(av[j])) {
+				cout << "3";
+			} else if (av[i]->isOneFour(av[j])) {
+				cout << "4";
+			} else {
+				cout << "-";
+			}
+		}
+		cout << endl;
+	}
+
+	av[11]->setUnboundFrom(av[6]);	// 2C 2CA
+
+	cout << "==========================" << endl;
+	cout << "Removed bond between atoms 2CA-2C" << endl;
+	for (unsigned int i=0; i<av.size(); i++) {
+		char c [1000];
+		sprintf(c, "%04d", i);
+		cout << c << " " << *av[i] << "  : ";
+		for (unsigned int j=0; j<av.size(); j++) {
+			if (i == j) {
+				cout << "1";
+			} else if (av[i]->isBoundTo(av[j])) {
+				cout << "2";
+			} else if (av[i]->isOneThree(av[j])) {
+				cout << "3";
+			} else if (av[i]->isOneFour(av[j])) {
+				cout << "4";
+			} else {
+				cout << "-";
+			}
+		}
+		cout << endl;
+	}
+
+	av[11]->setUnboundFrom(av[13]);	// 2C 3N
+
+	cout << "==========================" << endl;
+	cout << "Removed bond between atoms 2C-3N" << endl;
+	for (unsigned int i=0; i<av.size(); i++) {
+		char c [1000];
+		sprintf(c, "%04d", i);
+		cout << c << " " << *av[i] << "  : ";
+		for (unsigned int j=0; j<av.size(); j++) {
+			if (i == j) {
+				cout << "1";
+			} else if (av[i]->isBoundTo(av[j])) {
+				cout << "2";
+			} else if (av[i]->isOneThree(av[j])) {
+				cout << "3";
+			} else if (av[i]->isOneFour(av[j])) {
+				cout << "4";
+			} else {
+				cout << "-";
+			}
+		}
+		cout << endl;
 	}
 
 
