8a14efa62f55491e84e64b8307b7265959988a8c
Dan Kulp <dwkulp@gmail.com>
2010-05-18 05:18:29 +0000

    coiled coil and symmetric bundle example

---
+++ examples/example_coiled_coils_and_symmetric_bundles.cpp
@@ -0,0 +1,100 @@
+/*
+----------------------------------------------------------------------------
+This file is part of MSL (Molecular Software Libraries)
+ Copyright (C) 2010 Dan Kulp, Alessandro Senes, Jason Donald, Brett Hannigan,
+ Sabareesh Subramaniam, Ben Mueller
+
+This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, 
+ USA, or go to http://www.gnu.org/copyleft/lesser.txt.
+----------------------------------------------------------------------------
+*/
+#include <iostream>
+#include <cstdlib>
+
+#include "AtomContainer.h"
+#include "System.h"
+#include "MslTools.h"
+#include "CoiledCoils.h"
+#include "Symmetry.h"
+
+
+using namespace std;
+using namespace MSL;
+
+int main(int argc, char *argv[]) {
+
+	// the program requires the location of the "exampleFiles" as an argument
+	if (argc < 1) {
+		cerr << "USAGE:\nexample_coiled_coil_and_symmetric_bundles" << endl;
+		exit(0);
+	}
+
+	cout << "  ***************************************************************************************" << endl;
+	cout << "" << endl;
+	cout << "     How to generate coiled-coils in MSL (" << MslTools::getMSLversion() << ")   " << endl;
+	cout << "" << endl;
+	cout << "  ***************************************************************************************" << endl;
+	cout << endl;
+	cout << endl;
+
+	// CoiledCoils object used to create a coiled helix (by a number of algorithms)
+	CoiledCoils cc;
+
+	// A super-helical radius
+	double sr = 6.5;
+
+	// An alpha-helical phase angle
+	double aph = 0.0;
+
+	// A super-helical pitch angle, and pitch distance
+	double shpa = 190;
+	double shPitch = (2*M_PI*sr)/tan(M_PI*shpa/180);
+
+	// Hard code values of h (rise/residue) = 1.51, r1 (alpha-helical radius), and theta (alpha helical frequency)
+	double risePerResidue = 1.51;
+
+        // Use observed medians by Gevorg Grigoryan (Probing Deisgnability via a Generalized Model of Helical Bundle Geometry, JMB 2010)
+	double alphaHelicalRadius = 2.26;
+	double alphaHelicalFrequency = 102.8;
+
+	// Number of residues in coil ( lets do 4 heptads = 28 residues )
+	double numberOfResidues = 28;
+
+	// Generate a coiled coil, using specified parameters
+    	cc.northCoiledCoils(sr, risePerResidue, shPitch, alphaHelicalRadius, numberOfResidues, alphaHelicalFrequency, aph);
+
+	// Get the atoms from the CoiledCoils object back (this is a single coiled-coil helix)
+	AtomPointerVector coil = cc.getAtomPointers();
+
+	cout << "Writing /tmp/singleHelixCoil.pdb"<<endl;
+	PDBWriter pout;
+	pout.open("/tmp/singleHelixCoil.pdb");
+	pout.write(coil);
+	pout.close();
+
+	// Create a symmtery object to generate a coiled bundle ( C4 symmetric )
+	Symmetry sym;
+
+	// Apply C4 to "coil"
+	sym.applyCN(coil,4);
+	cout << "Writing /tmp/C4HelixCoil.pdb"<<endl;
+	pout.open("/tmp/C4HelixCoil.pdb");
+	pout.write(sym.getAtomPointers());
+	pout.close();
+
+	
+}
+
+
--- examples/examples.mk
+++ examples/examples.mk
@@ -1,5 +1,7 @@
 
 EXAMPLES = example_multipleAtomsCoordinates example_multipleResidueIdentities example_SasaCalculator_usage example_read_write_PDBs_with_the_AtomContainer \
            example_read_write_PDBs_with_the_System example_looping_over_Chain_Residues_Atoms example_AtomPointerVector \
-           example_selecting_atoms example_molecular_alignment example_regular_expressions example_measurements
+           example_selecting_atoms example_molecular_alignment example_regular_expressions example_measurements \
+	   example_coiled_coils_and_symmetric_bundles
+
 
