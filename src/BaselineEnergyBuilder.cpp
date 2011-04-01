8db32d6b42f3c35b3bfa232f4403678742f01004
Sabareesh Subramaniam <sabareeshs@gmail.com>
2011-04-01 22:07:36 +0000

    Creates BaselineInteractions for the given system.

---
+++ src/BaselineEnergyBuilder.cpp
@@ -0,0 +1,137 @@
+/*
+----------------------------------------------------------------------------
+This file is part of MSL (Molecular Software Libraries)
+ Copyright (C) 2011 Dan Kulp, Alessandro Senes, Jason Donald, Brett Hannigan,
+ Sabareesh Subramaniam, Ben Mueller
+
+This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, 
+ USA, or go to http://www.gnu.org/copyleft/lesser.txt.
+----------------------------------------------------------------------------
+*/
+
+
+#include "BaselineEnergyBuilder.h"
+
+using namespace MSL;
+using namespace std;
+
+
+BaselineEnergyBuilder::BaselineEnergyBuilder() {
+	setup();
+}
+
+BaselineEnergyBuilder::BaselineEnergyBuilder(System & _system, string _baselineFile) {
+	setup();
+	pSystem = &_system;
+	readParameters(_baselineFile);
+}
+BaselineEnergyBuilder::BaselineEnergyBuilder(System & _system, map<string,map<string,double> >& _pBaselineEnergies) {
+	setup();
+	pSystem = &_system;
+	pBaselineEnergies = _pBaselineEnergies;
+}
+BaselineEnergyBuilder::BaselineEnergyBuilder( BaselineEnergyBuilder & _builder) {
+	setup();
+	copy(_builder);
+}
+
+BaselineEnergyBuilder::~BaselineEnergyBuilder() {
+}
+
+void BaselineEnergyBuilder::operator=( BaselineEnergyBuilder & _builder) {
+	copy(_builder);
+}
+
+
+void BaselineEnergyBuilder::setup() {
+	pSystem = NULL;
+	pBaselineEnergies.clear();
+}
+
+void BaselineEnergyBuilder::reset() {
+	setup();
+}
+
+void BaselineEnergyBuilder::copy( BaselineEnergyBuilder & _builder) {
+	reset();
+	pSystem = _builder.pSystem;
+	pBaselineEnergies = _builder.pBaselineEnergies;
+}
+
+bool BaselineEnergyBuilder::readParameters(std::string _baselineFile) {
+	Reader pBaselineReader(_baselineFile);
+	pBaselineReader.open();
+	if (!(pBaselineReader.is_open())) {
+		cerr << "WARNING: Unable to open " << _baselineFile << endl;
+		return false;
+	} 
+
+	pBaselineEnergies.clear();
+		
+	vector<string> lines = pBaselineReader.getAllLines();
+
+	for(int i = 0; i < lines.size(); i++) {
+	//	cout << "UUU Line " << lines[i] << endl;
+
+		lines[i] = MslTools::uncomment(lines[i],"!");
+		vector <string> tokens = MslTools::tokenize(lines[i]," \t");
+		if(tokens.size() < 1) {
+			continue;
+		}
+		if(tokens.size() != 3 ) { 
+			cerr << "WARNING 14353 : Line \"" << lines[i] << "\" is not in FORMAT: ResName(string) AtomName(string) Energy(double) " << endl; 
+			continue;
+		}
+		pBaselineEnergies[MslTools::toUpper(tokens[0])][MslTools::toUpper(tokens[1])] = MslTools::toDouble(tokens[2]);
+	}
+
+	pBaselineReader.close();
+	return true;
+}
+
+void BaselineEnergyBuilder::printParameters () {
+	for(map<string,map<string,double> >::iterator it = pBaselineEnergies.begin(); it != pBaselineEnergies.end(); it++) {
+		for(map<string,double>::iterator it1 = (it->second).begin(); it1 != (it->second).end(); it1++) {
+			cout << it->first << " " << it1->first << " " << it1->second << endl;
+		}
+	}
+}
+
+bool BaselineEnergyBuilder::buildInteractions() {
+	vector<Position*>& positions = pSystem->getPositions();
+	EnergySet* ESet = pSystem->getEnergySet();
+	//cout << "UUUU Size: " << atoms.size() << endl;
+
+	for(vector<Position*>::iterator p = positions.begin(); p != positions.end(); p++) {
+		for(unsigned int i = 0; i < (*p)->identitySize(); i++) {
+			Residue& res = (*p)->getIdentity(i);
+			string resName = res.getResidueName();
+			if(pBaselineEnergies.find(resName) == pBaselineEnergies.end()) {
+				continue;
+			}
+			for(map<string,double>::iterator it = pBaselineEnergies[resName].begin(); it != pBaselineEnergies[resName].end(); it++) {
+				Atom *a = NULL;
+				if(res.atomExists(it->first)) {
+					a = &res.getLastFoundAtom();
+				} else {
+					continue;
+				}
+				ESet->addInteraction(new BaselineInteraction(*a,it->second));
+			}
+		}
+	}
+	return true;
+}
+
---
+++ src/BaselineEnergyBuilder.h
@@ -0,0 +1,74 @@
+/*
+----------------------------------------------------------------------------
+This file is part of MSL (Molecular Software Libraries)
+ Copyright (C) 2011 Dan Kulp, Alessandro Senes, Jason Donald, Brett Hannigan,
+ Sabareesh Subramaniam, Ben Mueller
+
+This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, 
+ USA, or go to http://www.gnu.org/copyleft/lesser.txt.
+----------------------------------------------------------------------------
+*/
+
+
+#ifndef BASELINEENERGYBUILDER_H
+#define BASELINEENERGYBUILDER_H
+
+#include <iostream>
+#include <map>
+#include <string>
+
+#include "System.h"
+#include "BaselineInteraction.h"
+
+namespace MSL { 
+	class BaselineEnergyBuilder {
+		public:
+			BaselineEnergyBuilder();
+			BaselineEnergyBuilder(System & _system, std::string _baselineFile);
+			BaselineEnergyBuilder(System & _system, std::map<std::string,std::map<std::string,double> >& _pBaselineEnergies);
+			BaselineEnergyBuilder( BaselineEnergyBuilder & _builder);
+			~BaselineEnergyBuilder();
+
+			void operator=( BaselineEnergyBuilder & _builder);
+
+			void setSystem(System & _system);
+
+			bool readParameters(std::string _baselineFile);
+
+			bool buildInteractions();
+
+			void printParameters();
+			std::map<std::string,std::map<std::string,double> > getBaselineEnergies();
+
+		private:
+			void setup();
+			void copy(BaselineEnergyBuilder & _builder);
+			void reset();
+
+			System * pSystem;
+
+			std::map<std::string,std::map<std::string,double> > pBaselineEnergies; // [ARG][CA] = 100.00
+
+	};
+	inline void BaselineEnergyBuilder::setSystem(System & _system) {
+		reset();
+		pSystem = &_system;
+	}
+	inline std::map<std::string,std::map<std::string,double> > BaselineEnergyBuilder::getBaselineEnergies() {
+		return pBaselineEnergies;
+	}
+}
+
+#endif
