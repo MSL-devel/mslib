d446c25e6953db0574baaf70cb066636e39f0725
Sabareesh Subramaniam <sabareeshs@gmail.com>
2011-04-01 22:07:23 +0000

    This interaction is used to specify a baseline energy for each residue type in a protein. It is a oneBodyInteraction.

---
+++ src/BaselineInteraction.cpp
@@ -0,0 +1,77 @@
+/*
+----------------------------------------------------------------------------
+This file is part of MSL (Molecular Software Libraries)
+ Copyright (C) 2011 Dan Kulp, Alessandro Senes, Jason Donald, Brett Hannigan,
+ Sabareesh Subramaniam, Ben Mueller
+
+This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, 
+ USA, or go to http://www.gnu.org/copyleft/lesser.txt.
+----------------------------------------------------------------------------
+*/
+
+
+#include "BaselineInteraction.h"
+
+using namespace MSL;
+using namespace std;
+const string BaselineInteraction::typeName = "BASELINE";
+
+BaselineInteraction::BaselineInteraction() {
+	setup(NULL,0.0);
+}
+
+BaselineInteraction::BaselineInteraction(Atom & _d1, double _energy) {
+	setup (&_d1,_energy);
+}
+
+BaselineInteraction::BaselineInteraction(const BaselineInteraction & _interaction) {
+	setup(NULL, 0.0);
+	copy(_interaction);
+}
+
+BaselineInteraction::~BaselineInteraction() {
+}
+
+
+void BaselineInteraction::setup(Atom * _pA1,double _energy) {
+	pAtoms.push_back(_pA1);
+	energy = _energy;
+}
+
+void BaselineInteraction::copy(const BaselineInteraction & _interaction) {
+	pAtoms = _interaction.pAtoms;
+	energy = _interaction.energy;	
+}
+
+void BaselineInteraction::printParameters() {
+	if(pAtoms.size() > 0 && pAtoms[0]) {
+		cout << " ResName " << pAtoms[0]->getResidueName() << endl;
+		cout << " atomName " << pAtoms[0]->getName() << endl;
+		cout << " energy " << energy << endl;
+	} else {
+		cout << "No atoms defined in interaction" << endl;
+	}
+}
+
+double BaselineInteraction::getEnergy(double _dummy) {
+	cerr << "WARNING 12334: BaselineInteraction::getEnergy(double _dummy) is not implemented" << endl;
+	return 100.0;
+
+}
+
+double BaselineInteraction::getEnergy() {
+	return(energy);
+}
+
---
+++ src/BaselineInteraction.h
@@ -0,0 +1,80 @@
+/*
+----------------------------------------------------------------------------
+This file is part of MSL (Molecular Software Libraries)
+ Copyright (C) 2011 Dan Kulp, Alessandro Senes, Jason Donald, Brett Hannigan,
+ Sabareesh Subramaniam, Ben Mueller
+
+This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, 
+ USA, or go to http://www.gnu.org/copyleft/lesser.txt.
+----------------------------------------------------------------------------
+*/
+
+
+#ifndef BASELINEINTERACTION_H
+#define BASELINEINTERACTION_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "OneBodyInteraction.h"
+#include "Atom.h"
+
+
+namespace MSL { 
+	class BaselineInteraction: public OneBodyInteraction {
+
+		/*******************************************************
+		 *   Inherits from OneBodyInteraction (a prototype object
+		 *   for the interaction of one atom)
+		 *******************************************************/
+
+		public:
+			BaselineInteraction();
+			BaselineInteraction(Atom & _d1, double _energy);
+
+			// should implement an operator= as well 
+			BaselineInteraction(const BaselineInteraction & _interaction);
+			~BaselineInteraction();
+
+			double getEnergy(double _dummy);
+			double getEnergy();
+			std::string toString() const;
+			void printParameters();
+
+			//unsigned int getType() const;
+			std::string getName() const;
+			friend std::ostream & operator<<(std::ostream &_os, BaselineInteraction & _term) {_os << _term.toString(); return _os;};
+
+					
+		private:
+			void setup(Atom * _d1, double _energy);
+			void copy(const BaselineInteraction & _interaction);
+			static const std::string typeName;
+	};
+
+	inline std::string BaselineInteraction::toString() const { 
+		if(pAtoms.size() && pAtoms[0]) {
+			char c [1000]; 
+			sprintf(c, "BASELINE %s %s %9.4f", pAtoms[0]->toString().c_str(),pAtoms[0]->getResidueName().c_str(),energy); 
+			return (std::string)c; 
+		}
+		return "";
+	};
+	inline std::string BaselineInteraction::getName() const {return typeName;}
+}
+
+#endif
+
