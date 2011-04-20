582041a88a33e0d06d595e77dcf3c4771a9a5e66
Dan Kulp <dwkulp@gmail.com>
2011-04-20 19:40:20 +0000

    new files

---
+++ programs/mutate.cpp
@@ -0,0 +1,176 @@
+/*
+----------------------------------------------------------------------------
+This file is part of MSL (Molecular Software Libraries)
+ Copyright (C) 2010 Dan Kulp, Alessandro Senes, Jason Donald, Brett Hannigan,
+ Sabareesh Subramaniam, Ben Mueller
+
+This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, 
+ USA, or go to http://www.gnu.org/copyleft/lesser.txt.
+----------------------------------------------------------------------------
+*/
+
+// MSL Includes
+#include "System.h"
+#include "OptionParser.h"
+#include "MslTools.h"
+#include "PolymerSequence.h"
+#include "Position.h"
+#include "Residue.h"
+#include "MslTools.h"
+#include "AtomicPairwiseEnergy.h"
+#include "PDBTopology.h"
+#include "mutate.h"
+
+// STL Includes
+#include <iostream>
+#include <string>
+#include <signal.h>
+
+using namespace MSL;
+using namespace std;
+
+
+
+int main(int argc, char *argv[]) {
+
+	Options opt = setupOptions(argc, argv);
+
+	cout << "Read in PDB structure"<<endl;
+	System sys;
+	sys.readPdb(opt.pdb);
+
+	// Get one of the residues 
+	Position &pos = sys.getPosition(opt.position);
+
+	PDBTopology pdbTop;
+
+	// Set a rotamer library and use atoms defined in the rotamer library to build
+	pdbTop.readRotamerLibrary(opt.rotlib);
+	pdbTop.setAddAtomsFromRotLib(true);
+
+	// Get backbone atoms  
+	AtomPointerVector backboneAtoms = pdbTop.getBackboneAtoms(pos.getCurrentIdentity());
+	
+	// *********  MUTATE *************** //
+	stringstream newPos;
+	newPos <<  opt.position <<","<<opt.newRes;
+	AtomContainer newAtoms = pdbTop.getResidue(newPos.str(),backboneAtoms,opt.numRot);
+
+	pos.addIdentity(newAtoms.getAtomPointers(),opt.newRes);
+	pos.setActiveIdentity(opt.newRes);
+
+	for (uint i = 0; i < pos.getTotalNumberOfRotamers();i++){
+		pos.setActiveRotamer(i);
+		char tmp[100];
+		sprintf(tmp,"%s-%03d.pdb",opt.outpdb.c_str(),i);
+	        string outfile(tmp);
+		sys.writePdb(outfile);
+	}
+
+}
+
+Options setupOptions(int theArgc, char * theArgv[]){
+
+    // Create the options
+    Options opt;
+    
+
+    // Parse the options
+    OptionParser OP;
+    OP.readArgv(theArgc, theArgv);
+    OP.setRequired(opt.required);    
+    OP.setAllowed(opt.optional);
+    //    OP.setShortOptionEquivalent(opt.equivalent);
+    OP.setDefaultArguments(opt.defaultArgs); // the default argument is the --configfile option
+    OP.autoExtendOptions(); // if you give option "solvat" it will be autocompleted to "solvationfile"
+
+
+    if (OP.countOptions() == 0){
+        cout << "Usage:" << endl;
+        cout << endl;
+        cout << "mutate CONF\n";
+        exit(0);
+    }
+
+    opt.configfile = OP.getString("configfile");
+    
+    if (opt.configfile != "") {
+        OP.readFile(opt.configfile);
+        if (OP.fail()) {
+            string errorMessages = "Cannot read configuration file " + opt.configfile + "\n";
+            cerr << "ERROR 1111 "<<errorMessages<<endl;
+        }
+    }
+
+    if (OP.getBool("help")){
+
+	    cout << "# PDB "<<endl;	
+	    cout << "pdb foo.pdb"<<endl<<endl;
+	    cout << "# Output pdb" <<endl;
+	    cout << "outpdb /tmp/out"<<endl<<endl;
+	    cout << "# Rotamer library"<<endl;
+	    cout << "rotlib /library/rotlib/balanced/rotlib-balanced-200.txt"<<endl<<endl;
+	    cout << "# Position"<<endl;
+	    cout << "position B,2"<<endl;
+	    cout << "# New residue"<<endl;
+	    cout << "newRes PHE"<<endl<<endl;
+	    cout << "# Number of rotamers"<<endl;
+	    cout << "numRot 10"<<endl<<endl;
+	    exit(1);
+    }
+
+
+    opt.pdb = OP.getString("pdb");
+    if (OP.fail()){
+	    cerr << "ERROR 1111 no pdb file"<<endl;
+    }
+
+    opt.rotlib = OP.getString("rotlib");
+    if (OP.fail()){
+	    cerr << "ERROR 1111 no rotlib file"<<endl;
+	    exit(1111);
+    }
+
+    opt.position = OP.getString("position");
+    if (OP.fail()){
+	    cerr << "ERROR 1111 no position"<<endl;
+	    exit(1111);
+    }
+    opt.newRes = OP.getString("newRes");
+    if (OP.fail()){
+	    cerr << "ERROR 1111 no new residue (newRes)"<<endl;
+	    exit(1111);
+    }
+
+    opt.outpdb = OP.getString("outpdb");
+    if (OP.fail()){
+	    opt.outpdb = "/tmp/out";
+	    cerr << "WARNING no outpdb file specifed will write to: "<<opt.outpdb<<endl;
+    }
+
+    opt.numRot = OP.getInt("numRot");
+    if (OP.fail()){
+	    cerr << "numRot defaults to 10"<<endl;
+	    opt.numRot = 10;
+    }
+    opt.debug = OP.getBool("debug");
+    if (OP.fail()){
+	    opt.debug = false;
+    }
+
+
+    
+    return opt;
+}
---
+++ programs/mutate.h
@@ -0,0 +1,67 @@
+/*
+----------------------------------------------------------------------------
+This file is part of MSL (Molecular Simulation Library)n
+ Copyright (C) 2009 Dan Kulp, Alessandro Senes, Jason Donald, Brett Hannigan
+
+This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, 
+ USA, or go to http://www.gnu.org/copyleft/lesser.txt.
+----------------------------------------------------------------------------
+*/
+#include <vector>
+struct Options {
+
+	// Set up options here...
+	Options(){
+
+		// Required
+		required.push_back("pdb");
+		required.push_back("rotlib");
+		required.push_back("position");
+		required.push_back("newRes");
+		required.push_back("numRot");
+
+		// Optional
+		optional.push_back("debug");
+		optional.push_back("outpdb");
+
+		// Configuration file..
+		defaultArgs.push_back("configfile");
+
+	}
+
+
+
+
+	// Storage for the vales of each option
+	std::string configfile;
+	std::string pdb;
+	std::string rotlib;
+	std::string position;
+	std::string newRes;
+	int numRot;
+	std::string outpdb;
+
+	bool debug;
+
+	// Storage for different types of options
+	std::vector<std::string> required;
+	std::vector<std::string> optional;
+	std::vector<std::string> defaultArgs;
+};
+
+
+Options setupOptions(int theArgc, char * theArgv[]);
+
+
