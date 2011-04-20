259fa47faddbdd25c924678880bb2b63a793be84
Brett T. Hannigan <brett@thehannigans.org>
2011-04-20 16:13:29 +0000

    Adding scripts

---
+++ scripts/miscUtils.py
@@ -0,0 +1,350 @@
+import glob
+import os
+import random
+
+################################################
+# The user passes in a filename, and this
+# function simply forms a list with an entry
+# per line of the file.  It will ignore lines
+# where the first non-whitespace char is a #.
+def loadListFromFile(fileName):
+    myList = []
+    
+    for line in open(fileName):
+        line = line.strip()
+        # Any line that has the first non-whitespace character
+        # as a # sign is treated as a comment.
+        if((len(line) > 0) and (line[0] != '#')):
+            myList += [line]
+    
+    return myList
+
+################################################
+# The user passes in either a directory or filename.
+# If a directory, then this function will return
+# all files in the given directory matching the
+# query sequence.  By default it will recursively
+# walk through all subdirectories.  If a file,
+# The function will simply extract all filenames
+# listed in the given file.
+def getFileList(dirOfInterestOrFile, query='*', recursive=True):
+    listOfFiles = []
+    # Recursively get all files in this directory and
+    # sub-directory.
+    if(os.path.isdir(dirOfInterestOrFile)):
+        if(recursive):
+            for path, dirs, files in os.walk(dirOfInterestOrFile):
+                listOfFiles += glob.glob( os.path.join(path, query) )
+        else:
+            listOfFiles += glob.glob( os.path.join(dirOfInterestOrFile, query) )
+    elif(os.path.isfile(dirOfInterestOrFile)):
+        for line in open(dirOfInterestOrFile):
+            listOfFiles += [line.strip()]
+
+    return listOfFiles
+
+################################################
+# The user passes in either a directory or filename.
+# If a directory, then this function will return
+# all directories in the given directory.
+# By default it will recursively
+# walk through all subdirectories.  If a file,
+# The function will simply extract all directories
+# listed in the given file.
+def getDirList(dirOfInterestOrFile, recursive=True):
+    listOfDirs = []
+    # Recursively get all directories in this directory and
+    # sub-directory.
+    if(os.path.isdir(dirOfInterestOrFile)):
+        if(recursive):
+            for path, dirs, files in os.walk(dirOfInterestOrFile):
+                listOfDirs += [path]
+        else:
+            temp = os.listdir(dirOfInterestOrFile)
+            for currTest in temp:
+                if(os.path.isdir(currTest)):
+                    listOfDirs += [currTest]
+    elif(os.path.isfile(dirOfInterestOrFile)):
+        for line in open(dirOfInterestOrFile):
+            listOfDirs += [line.strip()]
+
+    return listOfDirs
+
+
+################################################
+# This function will return a unique filename with
+# the given prefix, followed by some random digits
+def getRandomFileName(prefix):
+
+    fileName = ''
+    fileExists = True
+
+    # Loop until we find a unique filename.
+    while(fileExists):
+        # Choose some random 4 byte hex number.
+        randNum = hex(random.randint(0, 2**32 - 1))[2:]
+        # Append the random number to the prefix.
+        fileName = prefix + '_' + randNum
+        # Does this file already exist?
+        if(not (os.path.isfile(fileName) or os.path.isdir(fileName))):
+            fileExists = False
+
+    # Return unique filename.
+    return fileName
+
+##############################################
+# This function will poll on the given filename
+# looking for the given key word.  It will return
+# once it has seen the word.
+def pollOutputFile(outputFilename, keyWord):
+    while (not os.path.exists(outputFilename)):
+        a = 1
+
+    # Open file.
+    myFile = open(outputFilename)
+    notDone = True
+
+    # Loop until we see the key word.
+    while ( notDone ):
+        line = myFile.readline()
+        if (line and line.strip() == keyWord):
+            notDone = False
+
+##############################################
+# This function will poll on the given filename
+# looking for the given key word.  It will return
+# once it has seen the word.
+def launchCommandsInParallelAndPollForDone(commandList, queue):
+    magicWord = 'DONE'
+    # First create file names for each of our notifier files.
+    notifierFiles = []
+    for index in range(len(commandList)):
+        prefix = './_notifier_' + str(index)
+        notifierFiles += [getRandomFileName(prefix)]
+
+    # Now create new command list.
+    newCommandList = []
+    for index in range(len(commandList)):
+        newCommand = queue + ' -qsub "'
+        newCommand += commandList[index]
+        newCommand += ' ; echo "' + magicWord + '" > ' + notifierFiles[index] +'"'
+        newCommandList += [newCommand]
+
+    # Now actually launch the commands
+    for command in newCommandList:
+        print 'Launching ' + command
+        os.system(command)
+
+    # Poll for done and remove notifier file
+    # once we have seen it.
+    for notifierFile in notifierFiles:
+        print 'Polling on ' + notifierFile + '.'
+        pollOutputFile(notifierFile, magicWord)
+        os.remove(notifierFile)
+    
+
+##############################################
+def launchCommandsInParallelAndPollForDoneLocal(commandList, numConcurrentProcess):
+    import os
+    import stat
+    scriptFileNames = []
+
+    for i in range(numConcurrentProcess):
+        scriptFileNames += [getRandomFileName('_script_' + str(i))]
+    
+    currScriptIndex = 0
+    while(len(commandList) > 0):
+        currScriptFileName = scriptFileNames[currScriptIndex]
+        if(os.path.isfile(currScriptFileName) == False):
+            outFile = open(currScriptFileName, 'w')
+            outFile.write('#!/bin/sh\n')
+            outFile.write(commandList[0] + '\n')
+            outFile.write('rm ' + currScriptFileName + '\n')
+            outFile.close()
+            os.chmod(currScriptFileName, stat.S_IRWXU)
+            os.system(currScriptFileName + '&')
+            print 'Launching ' + commandList[0] + '.'
+            commandList = commandList[1:]
+
+        currScriptIndex = (currScriptIndex+1) % len(scriptFileNames)
+
+    filesStillExist = True
+    print 'Waiting for last commands to complete'
+    while(filesStillExist):
+        filesStillExist = False
+        for currScriptFileName in scriptFileNames:
+            filesStillExist = filesStillExist or os.path.isfile(currScriptFileName)
+    print 'DONE'
+
+##############################################
+# This function will loop over the arguments
+# given in argv and create a dictionary with
+# key value of the parameter name and value
+# of the given value.
+def parseCommandLineOptions(argv, convertToLower = True, preserveSlashDashes = True):
+    args = {}
+
+    # Some people use two dashes, some people use one dash.
+    import string
+
+    argv = string.join(argv).replace('--', '-')
+    if(preserveSlashDashes):
+        argv = argv.replace('\-', 'itsadash!')
+    
+    options = argv.strip().split('-')
+
+    for currOption in options:
+        # If the current option is empty, ignore.
+        if(currOption != ''):
+            words = currOption.split()
+            key = words[0]
+            if(convertToLower):
+                key = key.lower()
+            val = ['']
+            if(len(words) > 1):
+                val = words[1:]
+
+            if(preserveSlashDashes):
+                temp = []
+                for w in val:
+                    w = w.replace('itsadash!', '-')
+                    temp += [w]
+                val = temp
+                
+            args[key] = val
+
+    return args
+
+##############################################
+# This function will loop over the arguments
+# given in argv and create a dictionary with
+# key value of the parameter name and value
+# of the given value.
+def parseCommandLineOptionsFromFile(fn, convertToLower = True):
+    args = {}
+    import os
+    if(os.path.exists(fn)):
+        commandLineEquivalent = ''
+        for l in open(fn):
+            w = l.strip().split()
+
+            if( (len(w) >= 1) and (w[0][0] != '#') ):
+                commandLineEquivalent += '--' + w[0] + ' '
+                if(len(w) > 1):
+                    commandLineEquivalent += w[1].replace('-', '\-') + ' '
+
+        args = parseCommandLineOptions(fn, convertToLower, True)
+
+    return args
+
+################################################################################
+# This function will print the expected usage of the given script.
+def print_usage(OPTIONS, scriptName, missingKey = ''):
+    if(missingKey == ''):
+        print 'Missing the required option ' + missingKey + '.'
+
+    requiredArgs = ''
+    optionalArgs = ''
+    for key in OPTIONS:
+        if(OPTIONS[key][0]):
+            requiredArgs += '    --' + key + ' (' + OPTIONS[key][1] + ')\n'
+        else:
+            optionalArgs += '    --' + key + ' (' + OPTIONS[key][1] + ')\n'
+
+    print 'Usage: python ' + scriptName
+    print '  Required Arguments:'
+    print requiredArgs.rstrip()
+    print '  Optional Arguments:'
+    print optionalArgs.rstrip()
+
+
+################################################################################
+# This funciton will check that we have values for all of the required options.
+# It will aslo return a dictionary with the proper args.
+def check_options(argv, OPTIONS, scriptName):
+    args = {}
+    import sys
+    if(len(argv) == 1):
+        print_usage(OPTIONS, scriptName)
+        sys.exit(1)
+    elif(len(argv) == 2):
+        args = parseCommandLineOptionsFromFile(argv[1])
+    else:
+        args = parseCommandLineOptions(argv)
+
+    for key in OPTIONS:
+        if(OPTIONS[key][0] and (key not in args)):
+            print 'Missing ' + key + '.'
+            print_usage(OPTIONS, scriptName, key)
+            sys.exit(1)
+        elif( (len(OPTIONS[key]) == 3) and (key not in args)):
+               args[key] = OPTIONS[key][2]
+        elif(key in args):
+            temp = args[key]
+            # If there was no var for this arg, then it will
+            # be treated as a binary variable, and set to True.
+            if(temp == ['']):
+                args[key] = True
+            else:
+                args[key] = ' '.join(temp)
+
+    return args
+
+##############################################
+# This function will split the given list
+# into numSplits different lists
+def splitList(myList, numSplits):
+    import math
+    result = []
+
+    numElements = len(myList)
+    numElementsPerSplit = int( math.floor( float(numElements) / numSplits) )
+
+    for i in range(numSplits-1):
+        result += [myList[i*numElementsPerSplit:(i+1)*numElementsPerSplit]]
+
+    result += [myList[(numSplits-1)*numElementsPerSplit:]]
+
+    return result
+
+##############################################
+# This function will delete all jobs running
+# on the pbs system from the current user.
+def killAllPbsJobs():
+    import getpass
+    import subprocess
+
+    currUser = getpass.getuser()
+    tempFileName = getRandomFileName('_pbsq_info_')
+
+    command = 'pbsq -qstat | grep ' + currUser + ' > ' + tempFileName
+    subprocess.call(command, shell=True)
+
+    pbsIds = []
+    for line in open(tempFileName):
+        index = line.find('.pbsserver')
+        if(index > 0):
+            pbsIds += [line[0:index]]
+
+    for currId in pbsIds:
+        command = 'pbsq -qdel ' + currId
+        subprocess.call(command, shell=True)
+
+    os.remove(tempFileName)
+
+################################################
+# This function will find all instances of
+# the given string in a file and replace it
+# with the new string.
+def findAndReplaceInFile(fileName, f, r):
+    import shutil
+    import os
+
+    outFileName = getRandomFileName(fileName)
+    outFile = open(outFileName, 'w')
+    for line in open(fileName):
+        newLine = line.replace(f, r)
+        outFile.write(newLine)
+    outFile.close()
+    shutil.copyfile(outFileName, fileName)
+    os.remove(outFileName)
---
+++ scripts/submit.py
@@ -0,0 +1,308 @@
+import miscUtils
+import sys
+import os
+import subprocess
+import getpass
+import shutil
+import mslBuildTools
+import string
+
+fileListFileName = 'svn_submit_file.txt'
+sharedSubmitDir = '/tmp'
+failedSubmitDir = os.path.join(sharedSubmitDir, 'failed')
+releaseFile = os.path.join('src','release.h')
+
+
+########################################################
+# This will check to see that the proper arguments
+# were given and print a usage message if not.
+def testOptionsAndPrintUsage(options):
+    filesAndMessageSet = ('f' in options) and ('m' in options)
+    nowSetAndFileExists = ('now' in options) and os.path.isfile(fileListFileName)
+    restartOrNukeExists = ('restart' in options) or ('nukefaileddir' in options)
+
+    if( (filesAndMessageSet or nowSetAndFileExists or restartOrNukeExists) == False):
+        print 'Usage: submit -f <list of files> -m <file descriptions>'
+        print '       submit -now (when you have entered all of the files you wish to add.)'
+        print '   or  submit -restart (this will allow you to restart your submit filelist)'
+        print '   or  submit -nukefaileddir (this will delete all files from your failed directory'
+        sys.exit(1)
+
+########################################################
+# This function will append the filenames and messages
+# given on the command line to the file used to
+# actually submit.
+def addFilesAndMessagesToFileListFile(fullFileListFileName, filenames, messages):
+    fileList = ''
+    for filename in filenames:
+        fileList += '"' + filename + '",'
+    fileList = fileList[0:-1]
+
+
+    fileListFile = open(fullFileListFileName, 'a')
+    fileListFile.write('[[' + fileList + '],"' + string.join(messages) + '"]\n')
+    fileListFile.close()
+
+########################################################
+# This function will create and sync the given dir.
+def createAndSyncDir(newDirName):
+    os.mkdir(newDirName)
+    os.chdir(newDirName)
+    #command = 'cvs co msl'
+    command = 'svn co https://mslib.svn.sourceforge.net/svnroot/mslib/trunk'
+    subprocess.call(command,shell=True)
+
+########################################################
+# This function will open the given file name,
+# and take note of all of the files that will be modified.
+def getFilesToModify(fullFileListFileName):
+    filesToModify = []
+    
+    for line in open(fullFileListFileName):
+        temp = eval(line.strip())
+        filesToModify += temp[0]
+
+    return filesToModify
+
+########################################################
+# This function will submit the files.
+def submitFiles(newMslDirName, fileListFileName, newVersion, releaseFileName):
+    for line in open(fileListFileName):
+        temp = eval(line.strip())
+        #command = 'cvs commit -m "' + temp[1] + '" '
+        command = 'svn commit -m "' + temp[1] + '"'
+
+        for currFile in temp[0]:
+            command += ' ' + currFile
+
+        print 'Commiting with the following command: ' + command
+        subprocess.call(command, shell=True)
+
+    command = 'svn commit -m "Rolling release file." ' + releaseFileName
+    print 'Commiting with the following command: ' + command
+    subprocess.call(command, shell=True)
+
+    command = 'svn copy -m "Copying version." https://mslib.svn.sourceforge.net/svnroot/mslib/trunk https://mslib.svn.sourceforge.net/svnroot/mslib/tags/' + newVersion
+    print 'Copying repository with the following command: ' + command
+    subprocess.call(command, shell=True)
+
+
+########################################################
+# Copy the files from the current tree to the test tree.
+def copyMyFiles(cwd, mslDir, myFiles):
+    for currFile in myFiles:
+        srcFullFilePath = os.path.join(cwd, currFile)
+        dstFullFilePath = os.path.join(mslDir, currFile)
+        shutil.copy2(srcFullFilePath, dstFullFilePath)
+
+########################################################
+# This function will look to see what files and dirs
+# are new, and therefore need to be added.
+def addFilesAndDirectories(cwd, newMslDirName, myFiles):
+    errFileName = 'submit.err'
+    filesToBeAdded = []
+    os.chdir(newMslDirName)
+
+    for currFile in myFiles:
+        # First test to see if the directory even exists.
+        # If not, add it.
+        dirName = os.path.split(currFile)[0]
+        fullDirName = os.path.join(newMslDirName,dirName)
+
+        if(os.path.isdir(fullDirName) == False):
+            os.mkdir(fullDirName)
+            #command = 'cvs add ' + dirName
+            command = 'svn add ' + dirName
+            subprocess.call(command, shell=True)
+
+        # Now check to see if the file exists.  
+        if(os.path.isfile(currFile) == False):
+            filesToBeAdded += [currFile]
+
+    # Change back to the original directory
+    os.chdir(cwd)
+    return filesToBeAdded
+
+########################################################
+# This function will look in the release file to 
+# find the release version number.
+def getVersionNumber(releaseFilename):
+    version = ''
+    for line in open(releaseFilename):
+        line = line.strip()
+        if(line.find('MSLVERSION') > 0):
+            words = line.split()
+            if(len(words) >= 3):
+                version = words[2].strip('"')
+
+    return version
+
+    
+########################################################
+# This function will add the given files.
+def addFiles(cwd, newMslDirName, filesToBeAdded):
+    os.chdir(newMslDirName)
+
+    for currFile in filesToBeAdded:
+        #command = 'cvs add ' + currFile
+        command = 'svn add ' + currFile
+        subprocess.call(command, shell=True)
+
+    os.chdir(cwd)
+
+########################################################
+# This function will simply take a long line of text
+# and split it into smaller lines for the release file.
+def createTextForReleaseFile(line):
+    out = ''
+    
+    nl = '               '
+    for word in line.split():
+        nl += ' ' + word
+        if(len(nl) > 125):
+            out += nl + '\n'
+            nl = '                '
+
+    return out + nl
+
+########################################################
+# This function will modify the release.h file.
+def modifyReleaseFile(releaseFileName, newVersionNumber, userName, fileListFileName):
+    import datetime
+    import shutil
+    now = datetime.date.today().strftime('%B %d, %Y')
+    
+    tempFileName = miscUtils.getRandomFileName(releaseFileName)
+    tempFile = open(tempFileName, 'w')
+    
+    seenHistory = False
+    for line in open(releaseFileName):
+        if(seenHistory == False):
+            if(line.find('MSLVERSION') >= 0):
+                line = '#define MSLVERSION "' + newVersionNumber + '"\n'
+            if(line.find('MSLDATE') >= 0):
+                line = '#define MSLDATE "' + now + '"\n'
+        
+        tempFile.write(line)
+        
+        if(line.find('HISTORY:') >= 0):
+            templine = newVersionNumber + '    ' + now + '    ' + userName + '\n'
+            
+            for submitline in open(fileListFileName):
+                #temp = eval(submitline.strip())[1]
+                temp = eval(submitline.strip())
+                files = str(temp[0]).strip('[]')
+                while(len(files) < 15):
+                    files = files + ' '
+                comment = temp[1]
+                temp = createTextForReleaseFile(files + ' -' + comment) + '\n'
+                templine += temp
+            tempFile.write(templine)
+            seenHistory = True
+            
+    tempFile.close()
+    
+    shutil.copyfile(tempFileName, releaseFileName)
+    os.remove(tempFileName)
+
+########################################################
+# This function will just make sure that the new
+# version is greater than the old version.
+def isNewVersionLegal(newVersion, myVersion):
+    newVersion = newVersion.split('.')
+    myVersion = myVersion.split('.')
+
+    nv = [int(b) for b in newVersion]
+    mv = [int(b) for b in myVersion]
+
+    if(len(nv) != len(mv)):
+        return False
+
+    for i in range(len(mv)):
+        if (nv[i] == mv[i]):
+            continue
+        if(nv[i] < mv[i]):
+            return False
+        if(nv[i] > mv[i]):
+            return True
+
+    return False
+
+########################################################
+# Main
+options = miscUtils.parseCommandLineOptions(sys.argv[1:])
+cwd = os.getcwd()
+# Append the files to the filelist
+fullFileListFileName = os.path.join(cwd, fileListFileName)
+
+if('restart' in options):
+    if(os.path.isfile(fullFileListFileName)):
+        os.remove(fullFileListFileName)
+
+if('nukefaileddir' in options):
+    command = 'rm -rf ' + failedSubmitDir
+    subprocess.call(command,shell=True)
+
+testOptionsAndPrintUsage(options)
+
+
+if(('now' not in options) and ('f' in options)):
+    print 'Note, submit is just queing the files for submit.'
+    print 'To actually continue the submit, please specify the -now option'
+
+if('f' in options):
+    addFilesAndMessagesToFileListFile(fullFileListFileName, options['f'], options['m'])
+
+# If now was specified on the command line,
+# then we want to actually do the submit now.
+if('now' in options):
+    myFiles = getFilesToModify(fullFileListFileName)
+
+    currUser = getpass.getuser()
+    newDirName = miscUtils.getRandomFileName(os.path.join(sharedSubmitDir,currUser))
+    createAndSyncDir(newDirName)
+    #newMslDirName = os.path.join(newDirName,'msl')
+    newMslDirName = os.path.join(newDirName,'trunk')
+    
+    myVersion = getVersionNumber(os.path.join(cwd, releaseFile))
+    topVersion = getVersionNumber(os.path.join(newMslDirName, releaseFile))
+    if(myVersion != topVersion):
+        print 'The version given in your release.h, ' + myVersion + ', does not match that at the top of the tree, ' + topVersion + '.'
+        print 'Please sync your tree to the top of the tree, merge any conflicting files, and try to submit again.'
+        print 'Your list of files to submit will be kept, so once the tree is synced and merged, just run submit.py -now again.'
+        sys.exit(1)
+    
+    versionLegal = False
+    while(versionLegal == False):
+        newVersion = raw_input('The current version number is: ' + myVersion + '\nWhat is the new version number?\n')
+        versionLegal = isNewVersionLegal(newVersion, myVersion)
+        if(versionLegal == False):
+            print 'The new version of ' + newVersion + ' is not legal.  Try again.\n'
+    
+    # Modify the release.h file and and add it to the list.
+    modifyReleaseFile(os.path.join(cwd, releaseFile), newVersion, currUser, fullFileListFileName)
+    myFiles += [releaseFile]
+
+    shutil.move(fullFileListFileName, newDirName)
+    mslBuildTools.editMakefileFor64bit(newMslDirName)
+
+    filesToBeAdded = addFilesAndDirectories(cwd, newMslDirName, myFiles)
+    copyMyFiles(cwd, newMslDirName, myFiles)
+    addFiles(cwd, newMslDirName, filesToBeAdded)
+
+    failedBuilds = 'submit.py'
+    try:
+        numProcesses = int(options['now'][0])
+        failedBuilds = mslBuildTools.attemptToBuildTargets(newMslDirName, mslBuildTools.buildTargets, numProcesses)
+    except:
+        failedBuilds = mslBuildTools.attemptToBuildTargets(newMslDirName, mslBuildTools.buildTargets)
+
+    if(failedBuilds == ''):
+        submitFiles(newMslDirName, os.path.join(newDirName, fileListFileName), newVersion, releaseFile)
+        subprocess.call('rm -rf ' + newDirName, shell=True)
+        print 'Submitted!'
+    else:
+        shutil.move(newDirName, failedSubmitDir)
+        newDir = os.path.join(failedSubmitDir,os.path.split(newDirName)[1])
+        print 'Submit failed on target: ' + failedBuilds + '.  Moved directory to ' + newDir + '.'
+        sys.exit(1)
