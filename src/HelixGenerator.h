911298529f3cda8d2f6c90f9dbe9d90dd4ea4220
Alessandro Senes <senes@wisc.edu>
2012-05-08 03:49:04 +0000

    Moved under GSL requirement the function getHelixAxis. NOTE: this is a global function, not an object function. Should be changed (its own object or MSL tools?). Does not seem to be used.

--- src/HelixGenerator.h
+++ src/HelixGenerator.h
@@ -1,7 +1,13 @@
 /*
 ----------------------------------------------------------------------------
-This file is part of MSL (Molecular Simulation Library)n
- Copyright (C) 2009 Dan Kulp, Alessandro Senes, Jason Donald, Brett Hannigan
+This file is part of MSL (Molecular Software Libraries) 
+ Copyright (C) 2008-2012 The MSL Developer Group (see README.TXT)
+ MSL Libraries: http://msl-libraries.org
+
+If used in a scientific publication, please cite: 
+Kulp DW et al. "Structural informatics, modeling and design with a open 
+source Molecular Software Library (MSL)" (2012) J. Comp. Chem, in press
+DOI: 10.1002/jcc.22968
 
 This library is free software; you can redistribute it and/or
  modify it under the terms of the GNU Lesser General Public
@@ -15,7 +21,7 @@ This library is distributed in the hope that it will be useful,
 
 You should have received a copy of the GNU Lesser General Public
  License along with this library; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, 
  USA, or go to http://www.gnu.org/copyleft/lesser.txt.
 ----------------------------------------------------------------------------
 */
@@ -69,7 +75,10 @@ private:
     double rise;
 };
 
+#ifdef __GSL__
+// Global function??? Is it ever used??? REMOVE?
 Line getHelixAxis(AtomPointerVector &_av);
+#endif
 
 
 }
