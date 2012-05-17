a39345bdb350651a68ec43c31d9d5d1a67975a92
Brett T. Hannigan <brett@thehannigans.org>
2012-05-17 19:25:21 +0000

    This allows you to run a test level.

---
+++ scripts/runTestLevel.py
@@ -0,0 +1,24 @@
+import mslBuildTools
+import sys
+import miscUtils
+import getpass
+import os
+
+OPTIONS = {'testlevel' :                  (True, 'The test level file.'),
+           'numprocesses' :               (False, 'How many processes should we use while compiling?', 1),}
+
+SHARED_SUBMIT_DIR = '/tmp'
+
+################################################################################
+args = miscUtils.check_options(sys.argv, OPTIONS)
+
+#currUser = getpass.getuser()
+#newDirName = miscUtils.getRandomFileName(os.path.join(SHARED_SUBMIT_DIR, currUser))
+#newMslDirName = os.path.join(newDirName, 'trunk')
+newMslDirName = './'
+
+import pdb
+pdb.set_trace()
+
+results = mslBuildTools.run_tests(newMslDirName, mslBuildTools.buildTargets, int(args['numprocesses']))
+mslBuildTools.print_test_results(results)
