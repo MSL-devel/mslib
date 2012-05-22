1c403eb02fc7df23db5321ad54d4fe33cde8c801
Alessandro Senes <senes@wisc.edu>
2012-05-22 00:23:31 +0000

    EZ potential single body interaction.

---
+++ src/EZpotentialInteraction.cpp
@@ -0,0 +1,84 @@
+/*
+----------------------------------------------------------------------------
+This file is part of MSL (Molecular Software Libraries) 
+ Copyright (C) 2008-2012 The MSL Developer Group (see README.TXT)
+ MSL Libraries: http://msl-libraries.org
+
+If used in a scientific publication, please cite: 
+Kulp DW et al. "Structural informatics, modeling and design with a open 
+source Molecular Software Library (MSL)" (2012) J. Comp. Chem, in press
+DOI: 10.1002/jcc.22968
+
+This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, 
+ USA, or go to http://www.gnu.org/copyleft/lesser.txt.
+----------------------------------------------------------------------------
+*/
+
+
+#include "EZpotentialInteraction.h"
+
+using namespace MSL;
+using namespace std;
+
+
+const string EZpotentialInteraction::typeName = "EZ_POTENTIAL";
+
+EZpotentialInteraction::EZpotentialInteraction() {
+	setup(NULL, vector<double>(3, 0.0), true);
+}
+
+EZpotentialInteraction::EZpotentialInteraction(Atom & _pA1, std::vector<double> _params, bool _sigmoidalFunction) {
+	setup (&_pA1, _params, _sigmoidalFunction);
+}
+
+EZpotentialInteraction::EZpotentialInteraction(const EZpotentialInteraction & _interaction) {
+	setup(NULL, vector<double>(3, 0.0), true);
+	copy(_interaction);
+}
+
+EZpotentialInteraction::~EZpotentialInteraction() {
+}
+
+void EZpotentialInteraction::setup(Atom * _pA1, vector<double> _params, bool _sigmoidalFunction) {
+	setParams(_params);
+	pAtoms = vector<Atom*> (1, (Atom*)NULL);
+	setAtoms(*_pA1);	
+	isSigmoidal_flag = _sigmoidalFunction;
+}
+
+void EZpotentialInteraction::copy(const EZpotentialInteraction & _interaction) {
+	pAtoms = _interaction.pAtoms;
+	params = _interaction.params;
+	isSigmoidal_flag = _interaction.isSigmoidal_flag;
+}
+
+double EZpotentialInteraction::getEnergy() {
+	return getEnergy(pAtoms[0]->getZ(), params, isSigmoidal_flag);
+}
+
+double EZpotentialInteraction::getEnergy(double _Zcoor, const vector<double> & _param, bool _sigmoidalFunction) const { 
+	if(_Zcoor < 0) 
+		_Zcoor = -1*_Zcoor;
+	if (_Zcoor > 25.0) {
+		_Zcoor = 25.0;
+	}
+	if (_sigmoidalFunction) {
+		return (_param[0] / ( 1 + (pow((_Zcoor/_param[1]), _param[2]))));
+	} else {
+		return _param[2]*exp(-((_Zcoor-_param[1])*(_Zcoor-_param[1])) / (2*_param[0]*_param[0]));
+	}
+}
+
+
---
+++ src/EZpotentialInteraction.h
@@ -0,0 +1,127 @@
+/*
+----------------------------------------------------------------------------
+This file is part of MSL (Molecular Software Libraries) 
+ Copyright (C) 2008-2012 The MSL Developer Group (see README.TXT)
+ MSL Libraries: http://msl-libraries.org
+
+If used in a scientific publication, please cite: 
+Kulp DW et al. "Structural informatics, modeling and design with a open 
+source Molecular Software Library (MSL)" (2012) J. Comp. Chem, in press
+DOI: 10.1002/jcc.22968
+
+This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, 
+ USA, or go to http://www.gnu.org/copyleft/lesser.txt.
+----------------------------------------------------------------------------
+*/
+
+
+#ifndef EZPOTENTIALINTERACTION_H
+#define EZPOTENTIALINTERACTION_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "OneBodyInteraction.h"
+
+/*******************************************************
+ *  This is an implementation of the EZ empirical membrane 
+ *  insertion potential.  If used please cite:
+ *
+ *     Senes A, Chadi DC, Law PB, Walters RF, Nanda V, DeGrado WF. 
+ *     "E(z), a depth-dependent potential for assessing the energies 
+ *     of insertion of amino acid side-chains into membranes, 
+ *     derivation and applications to determining the orientation 
+ *     of transmembrane and interfacial helices."
+ *     J Mol Biol. 2007 366(2), 436-48
+ *
+ *******************************************************/
+
+namespace MSL { 
+	class EZpotentialInteraction: public OneBodyInteraction {
+
+		/*******************************************************
+		 *   Inherits from OneBodyInteraction (a prototype object
+		 *   for the interaction of two atoms)
+		 *******************************************************/
+
+		public:
+			EZpotentialInteraction();
+			EZpotentialInteraction(Atom & _pA1, std::vector<double> _params, bool _sigmoidalFunction);
+
+			// should implement an operator= as well 
+			EZpotentialInteraction(const EZpotentialInteraction & _interaction);
+			~EZpotentialInteraction();
+
+			/* setting and getting the parameters */
+			void setParams(std::vector<double> _params);
+			std::vector<double> getParams() const;
+			void setSigmoidalFunction(bool _flag);
+			bool getSigmoidalFunction() const;
+			
+			double getEnergy();
+			double getEnergy(double _Zcoor, const std::vector<double> & _param, bool _sigmoidalFunction) const;
+			double getEnergy(std::vector<double> *paramDerivatives); // derivatives not implemented
+			double getEnergy(double _param, std::vector<double> *paramDerivatives=NULL); // derivatives not implemented
+			std::vector<double> getEnergyGrad(); // derivatives not implemented
+
+			friend std::ostream & operator<<(std::ostream &_os, EZpotentialInteraction & _term) {_os << _term.toString(); return _os;};
+			std::string toString() ;
+
+			std::string getName() const;
+			std::pair<double,std::vector<double> > partialDerivative();
+			
+		private:
+			void setup(Atom * _pA1, std::vector<double> _params, bool _sigmoidalFunction);
+			void copy(const EZpotentialInteraction & _interaction);
+
+			//static const unsigned int type = 2;
+			static const std::string typeName;
+			bool isSigmoidal_flag;
+			
+	};
+
+	inline void EZpotentialInteraction::setParams(std::vector<double> _params) { if (_params.size() != 3) {std::cerr << "ERROR 12357: invalid number of parameters in inline void EZpotentialInteraction::setParams(std::vector<double> _params, bool _sigmoidalFunction) = " << _params.size() << std::endl; exit(12357);} params = _params;}
+	inline std::vector<double> EZpotentialInteraction::getParams() const {return params;};
+	inline std::string EZpotentialInteraction::toString() { char c [1000]; sprintf(c, "%s %s %9.4f %9.4f %9.4f %u %20.6f", typeName.c_str(), pAtoms[0]->toString().c_str(), params[0], params[1], params[2], isSigmoidal_flag, getEnergy()); return (std::string)c; };
+	inline void EZpotentialInteraction::setSigmoidalFunction(bool _flag) {
+		isSigmoidal_flag = _flag;
+	}
+	inline bool EZpotentialInteraction::getSigmoidalFunction() const {
+		return isSigmoidal_flag;
+	}
+
+	inline std::string EZpotentialInteraction::getName() const {return typeName;}
+
+	inline std::pair<double,std::vector<double> > EZpotentialInteraction::partialDerivative() {
+		std::cerr << "ERROR 38984: std::pair<double,std::vector<double> > EZpotentialInteraction::partialDerivative() is not implemented" << std::endl;
+		exit(38984);
+	}
+	inline double EZpotentialInteraction::getEnergy(std::vector<double> *_dd) {
+		std::cerr << "ERROR 38989: double EZpotentialInteraction::getEnergy(std::vector<double> *_dd) is not implemented" << std::endl;
+		exit(38989);
+	}
+	inline double EZpotentialInteraction::getEnergy(double _param, std::vector<double> *paramDerivatives) {
+		std::cerr << "ERROR 38994: double EZpotentialInteraction::getEnergy(double _param, std::vector<double> *paramDerivatives) is not implemented" << std::endl;
+		exit(38994);
+	}
+	inline std::vector<double> EZpotentialInteraction::getEnergyGrad() {
+		std::cerr << "ERROR 38999: std::vector<double> EZpotentialInteraction::getEnergyGrad() is not implemented" << std::endl;
+		exit(38999);
+	}
+}
+
+#endif
+
