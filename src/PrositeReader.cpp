a219276e49787247b2e1626ef2ea654c2474bcf3
Dan Kulp <dwkulp@gmail.com>
2012-05-24 21:46:23 +0000

    Reader for prosite database

---
+++ src/PrositeReader.cpp
@@ -0,0 +1,112 @@
+/*
+----------------------------------------------------------------------------
+This file is part of MSL (Molecular Software Libraries)
+ Copyright (C) 2010 Dan Kulp, Alessandro Senes, Jason Donald, Brett Hannigan,
+ Sabareesh Subramaniam, Ben Mueller
+
+This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, 
+ USA, or go to http://www.gnu.org/copyleft/lesser.txt.
+----------------------------------------------------------------------------
+*/
+
+
+#include "PrositeReader.h"
+
+using namespace MSL;
+using namespace std;
+
+
+
+
+
+bool PrositeReader::read() {
+	if (!is_open()) {
+		cerr << "WARNING: 14567 PrositeReader::read () File not open" << endl;
+		return false;
+	}
+
+	try { 
+
+	       string id = "";
+	       string de = "";
+	       string pa = "";
+	       while (!endOfFileTest()){
+			string line = Reader::getLine();
+			// Skip comments
+			if (line.substr(0,2) == "CC") continue;
+
+			// New section...
+			if (line.substr(0,2) == "//") {
+
+			  // Add last entry
+			  if (id != "" && de != "" && pa != ""){
+			    prosite_data[id][de] = pa;
+			    id = "";
+			    de = "";
+			    pa = "";
+			  }
+			  continue;
+
+			}
+
+			// do line parsing
+			if (line.substr(0,2) == "ID"){
+			  vector<string> tokens = MslTools::tokenize(line, ";");
+			  if (tokens.size() < 2) {
+			    cerr << "ERROR PrositeReader::read() line: "<<line<<endl;
+			    exit(13423);
+			  }
+			  if (tokens[1] != " PATTERN.") continue;
+			  
+			  
+			  id = tokens[0].substr(2,tokens[0].length()-1);
+			  id = MslTools::trim(id);
+			  
+			}
+			if (line.substr(0,2) == "DE"){
+			  de = line.substr(2,line.length()-1);
+			  de = MslTools::trim(de);
+			}
+			if (line.substr(0,2) == "PA"){
+
+			  string pa_line = line.substr(2,line.length()-1);
+			  pa_line = MslTools::trim(pa_line);
+
+			  // Convert prosite expression to regular expression
+			  MslTools::replace(pa_line," ","",true);
+			  MslTools::replace(pa_line,".","",true);
+			  MslTools::replace(pa_line,"-","",true);
+			  MslTools::replace(pa_line,"x",".",true);
+			  MslTools::replace(pa_line,"{","[^",true);
+			  MslTools::replace(pa_line,"}","]",true);
+			  MslTools::replace(pa_line,"(","{",true);
+			  MslTools::replace(pa_line,")","}",true);
+			  MslTools::replace(pa_line,">","$",true);
+
+			  pa += pa_line;
+			}
+
+		
+		}
+		return true;
+		
+
+	} catch(...){
+		cerr << "ERROR 5623 in PrositeReader::read()\n";
+		return false;
+	}
+
+	return true;
+}
---
+++ src/PrositeReader.h
@@ -0,0 +1,137 @@
+/*
+----------------------------------------------------------------------------
+This file is part of MSL (Molecular Simulation Library)
+ Copyright (C) 2010 Dan Kulp, Alessandro Senes, Jason Donald, Brett Hannigan
+ Sabareesh Subramaniam, Ben Mueller
+
+This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, 
+ USA, or go to http://www.gnu.org/copyleft/lesser.txt.
+----------------------------------------------------------------------------
+*/
+
+#ifndef PROSITEREADER_H
+#define PROSITEREADER_H
+/*
+
+ */
+
+//MSL Includes
+#include "Reader.h"
+#include "MslTools.h"
+
+// Storage Formats
+#include "CartesianPoint.h"
+#include "AtomPointerVector.h"
+
+
+// STL Includes
+#include <vector>
+#include <map>
+
+/**
+ * This class will provide an object which is able
+ * to read in and interpret CRD files.
+ */
+namespace MSL { 
+class PrositeReader : public Reader {
+
+	public:
+		// Constructors/Destructors
+		PrositeReader();
+		PrositeReader(const std::string &_filename);
+		PrositeReader(const PrositeReader & _reader);
+		PrositeReader(std::stringstream &_stream);
+		virtual ~PrositeReader();
+		
+		bool read();
+		bool read(std::string &_inputString);
+
+		std::map<std::string,std::map<std::string, std::string> >& getPrositeData();
+
+	protected:		
+	private:
+		void setup();
+
+		// ID,DE,PA  (pattern id, pattern description, pattern regex string)
+		std::map<std::string, std::map<std::string, std::string> > prosite_data;
+
+};
+
+//Inlines go HERE
+/**
+ * Simple constructor.
+ */
+inline PrositeReader::PrositeReader() : Reader() { setup(); }
+/**
+ * With this constructor the user specifies the filename
+ * of the Prosite to be read.
+ *
+ * @param _filename  The name of the Prosite file to be read.
+ */
+inline PrositeReader::PrositeReader(const std::string &_filename) : Reader(_filename) { setup();}
+
+/**
+ * A copy constructor.  All of the atoms from the given PrositeReader are
+ * copied into the new PrositeReader.
+ *
+ * @param _reader The PrositeReader to be copied.
+ */
+inline PrositeReader::PrositeReader(const PrositeReader & _reader) {
+  prosite_data = _reader.prosite_data;
+}
+/**
+ * A constructor which will read input data from a std::stringstream.
+ *
+ * @param _ss The std::stringstream to get data from.
+ */
+inline PrositeReader::PrositeReader(std::stringstream &_ss) : Reader(_ss)     {
+	setup();
+	read();
+}
+
+inline void PrositeReader::setup() {
+
+  prosite_data.clear();
+}
+
+/**
+ * The deconstructor.  All data will be deleted, so any Atom pointers
+ * that were previously saved off will no longer be valid after the PrositeReader
+ * object has been destroyed.
+ */
+inline PrositeReader::~PrositeReader() { close();}
+
+
+
+
+
+inline bool PrositeReader::read(std::string &_inputString){
+	fileName = "string";
+	stringStreamPtr = new std::stringstream();
+	stringStreamPtr->str(_inputString);
+	fileHandler = stringstyle;
+
+	// Now call read..
+	return read();
+}
+
+ inline std::map<std::string,std::map<std::string, std::string> >& PrositeReader::getPrositeData(){
+   return prosite_data;
+ } 
+}
+
+
+#endif
+
