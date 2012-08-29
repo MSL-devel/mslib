84b9937ac01f809158ba7d831cabc63b6a5bdc61
Jason Donald <jedonald@gmail.com>
2012-08-29 13:50:58 +0000

    Update program code to have it give more flexible options and output, adding option for rotamer levels.

--- programs/runQuench.cpp
+++ programs/runQuench.cpp
@@ -26,78 +26,33 @@ You should have received a copy of the GNU Lesser General Public
  USA, or go to http://www.gnu.org/copyleft/lesser.txt.
 ----------------------------------------------------------------------------
 */
-
-
-#include <string>
-#include <sstream>
-#include <vector>
-#include <ostream>
-#include <fstream>
-#include <cmath>
-#include <algorithm>
-
-
 #include "runQuench.h"
-#include "Quench.h"
-#include "OptionParser.h"
-#include "PDBWriter.h"
-#include "System.h"
-#include "SystemRotamerLoader.h"
-#include "PairwiseEnergyCalculator.h"
-#include "AtomicPairwiseEnergy.h"
-#include "PolymerSequence.h"
-#include "CharmmSystemBuilder.h"
-
-using namespace std;
-
-using namespace MSL;
-
-
 
 int main(int argc, char *argv[]){
-
-	
 	// Option Parser
 	Options opt = setupOptions(argc,argv);
+
+	if(opt.errorFlag) {
+		cout << opt.OPerrors ;
+		cout << opt.errorMessages ;
+		exit(0);
+	}
+
+	if(opt.warningFlag) {
+		cout << opt.warningMessages;
+	}
 
 	System initialSystem;
 	initialSystem.readPdb(opt.pdb);
 
-	// Need to check for HIS residues.
-	for (uint i = 0 ; i < initialSystem.positionSize();i++){
-	  Residue &res = initialSystem.getPosition(i).getCurrentIdentity();
-	  if (res.getResidueName() == "HIS" &&
-	      res.atomExists("HE2") &&
-	      res.atomExists("HD1")){
-		fprintf(stdout,"Residue %10s is a protonated histidine change name to HSP\n",res.toString().c_str());
-		res.setResidueName("HSP");
-		continue;
-	  }
-
-	  if (res.getResidueName() == "HIS" &&
-	      res.atomExists("HE2")){
-		fprintf(stdout,"Residue %10s is a epsilon neutral histidine change name to HSE\n",res.toString().c_str());
-		res.setResidueName("HSE");
-	        continue;
-	  }
-
-	  if (res.getResidueName() == "HIS" &&
-	      res.atomExists("HD1")){
-		fprintf(stdout,"Residue %10s is a delta   neutral histidine change name to HSD\n",res.toString().c_str());
-		res.setResidueName("HSD");
-	        continue;
-	  }
-
-	  if (res.getResidueName() == "HIS"){
-	    cout << "Residue: "<<res.toString()<< " is HIS but hydrogen configuration is not specified\n";
-	    exit(2222);
-	  }
-	}
-
 	Quench quencher(opt.topfile, opt.parfile, opt.rotlib);
 
 	// Set the number of rotamers you want for large and small side chains, respectively
-	quencher.setVariableNumberRotamers(opt.largeRotNum,opt.smallRotNum);
+	if (opt.rotLevel != "") {
+		quencher.setRotamerLevel(opt.rotLevel);
+	} else {
+		quencher.setVariableNumberRotamers(opt.largeRotNum,opt.smallRotNum);
+	}
 
 	// Set which positions you want to repack (optional)
 	if (opt.positions.size() > 0) {	
@@ -173,7 +128,6 @@ int main(int argc, char *argv[]){
 
 	    }
 
-
 	    // Take the set difference between variable and ignore positions
 	    vector<int> uniquePositions;
 	    std::set_difference(variablePositions.begin(),variablePositions.end(),
@@ -184,9 +138,6 @@ int main(int argc, char *argv[]){
 	      cerr << "ERROR 2222 AutoFindPositions code did not find any clashing residues\n";
 	      exit(2222);
 	    }
-	
-	    
-
 
 	    cout << "Positions that clashed + their neighbors are: \n";
 	    for (uint v = 0; v < uniquePositions.size();v++){
@@ -196,20 +147,6 @@ int main(int argc, char *argv[]){
 
 	    System sys = quencher.runQuench(initialSystem,uniquePositions);
 
-	    // Rename HIS residues.
-	    /*
-	    for (uint i = 0 ; i < sys.positionSize();i++){
-	    	  Residue &res = sys.getPosition(i).getCurrentIdentity();
-		  if (res.getResidueName() == "HSP" ||
-		      res.getResidueName() == "HSD" ||
-		      res.getResidueName() == "HSE"){
-
-			fprintf(stdout,"Residue %10s is being renamed to HIS\n",res.toString().c_str());
-			res.setResidueName("HIS");
-			continue;
-		  }
-	    }
-	    */
 	    cout << "Write post quench pdb " << opt.outfile << endl;
 	    sys.writePdb(opt.outfile);
 
@@ -225,78 +162,6 @@ int main(int argc, char *argv[]){
 	  }
 	}
 
-
 	cout << "Done."<<endl;
 }
 
-
-Options setupOptions(int theArgc, char * theArgv[]){
-	Options opt;
-
-	OptionParser OP;
-
-	OP.readArgv(theArgc, theArgv);
-	OP.setRequired(opt.required);
-	OP.setAllowed(opt.optional);
-
-	if (OP.countOptions() == 0){
-		cout << "Usage:" << endl;
-		cout << endl;
-		cout << "runQuench --pdb PDB [--topfile TOPFILE --parfile PARFILE --rotlib ROTLIB --outfile OUTPDB --positions A_73 A_74 A_75 --smallRotNum NUM --largeRotNum NUM --autoFinPositions]\n";
-		exit(0);
-	}
-
-	opt.pdb  = OP.getString("pdb");
-	if (OP.fail()){
-		cerr << "ERROR 1111 pdb not specified.\n";
-		exit(1111);
-	}
-
-	opt.topfile = OP.getString("topfile");
-	if (OP.fail()){
-		cerr << "WARNING no topfile specified, using default /library/charmmTopPar/top_all22_prot.inp\n";
-		opt.topfile = "/library/charmmTopPar/top_all22_prot.inp";
-	}
-
-	opt.parfile = OP.getString("parfile");
-	if (OP.fail()){
-		cerr << "WARNING no parfile specified, using default /library/charmmTopPar/par_all22_prot.inp\n";
-		opt.parfile = "/library/charmmTopPar/par_all22_prot.inp";
-	}
-
-	opt.rotlib = OP.getString("rotlib");
-	if (OP.fail()){
-		cerr << "WARNING no rotlib specified, using default /library/rotlib/balanced/rotlib-balanced-200.txt\n";
-		opt.rotlib = "/library/rotlib/balanced/rotlib-balanced-200.txt";
-	}
-
-	opt.outfile = OP.getString("outfile");
-	if (OP.fail()){
-		cerr << "WARNING no outfile specified, using default /tmp/currentConformation.pdb\n";
-		opt.outfile = "/tmp/currentConformation.pdb";
-	}
-
-	opt.positions = OP.getStringVectorJoinAll("positions");
-
-	opt.largeRotNum = OP.getInt("largeRotNum");
-	if (OP.fail()){
-		cerr << "WARNING largeRotNum not specified, using default 50\n";
-		opt.largeRotNum = 50;
-	}
-
-	opt.smallRotNum = OP.getInt("smallRotNum");
-	if (OP.fail()){
-		cerr << "WARNING smallRotNum not specified, using default 5\n";
-		opt.smallRotNum = 5;
-	}
-	if (opt.largeRotNum < 0 || opt.smallRotNum < 0) { cerr << "Need a positive rotamer number" << endl; exit(15); }
-
-
-	opt.autoFindPositions = OP.getBool("autoFindPositions");
-	if (OP.fail()){
-	  opt.autoFindPositions = false;
-	}
-
-
-	return opt;
-}
--- programs/runQuench.h
+++ programs/runQuench.h
@@ -29,46 +29,242 @@ You should have received a copy of the GNU Lesser General Public
 
 #include <string>
 #include <vector>
+#include <sstream>
+#include <ostream>
+#include <fstream>
+#include <cmath>
+#include <algorithm>
+
+#include "Quench.h"
+#include "OptionParser.h"
+#include "PDBWriter.h"
+#include "System.h"
+#include "SystemRotamerLoader.h"
+#include "PolymerSequence.h"
+#include "CharmmSystemBuilder.h"
+#include "SysEnv.h"
+
+using namespace std;
+using namespace MSL;
+
+static SysEnv SYSENV;
+
+string programName = "runQuench";
+string programDescription = "This program further optimizes a protein structure by running a side chain quenching algorithm";
+string programAuthor = "Jason Donald";
+string programVersion = "1.0.2";
+string programDate = "Sep 18 2009";
+string mslVersion =  MSLVERSION;
+string mslDate = MSLDATE;
 
 // Define Input Options and Store them.
 struct Options {
-
-	// Set up options here...
-	Options(){
-
-
-		// The input PDB file..
-		required.push_back("pdb");
-
-		// Optional arguments
-		optional.push_back("topfile");
-		optional.push_back("parfile");
-		optional.push_back("rotlib");
-		optional.push_back("outfile");
-		optional.push_back("positions");
-		optional.push_back("largeRotNum");
-		optional.push_back("smallRotNum");
-		optional.push_back("autoFindPositions");
-	}
 
 	// Storage for the vales of each option
 	std::string pdb;
 	std::string topfile;
 	std::string parfile;
+	std::string configfile;
 	std::string rotlib;
 	std::string outfile;
+	std::string rotLevel;
 	std::vector<std::string> positions;
 	int largeRotNum;
 	int smallRotNum;
 	bool autoFindPositions;
 
+	bool version; // ask for the program version
+	bool help; // ask for the program help
+
+	bool errorFlag; // true if there are errors
+	bool warningFlag; // true if there are warnings
+	string errorMessages; // error messages
+	string warningMessages; //warning messages
+	string OPerrors; //the errors from the option parser
+
 	// Storage for different types of options
 	std::vector<std::string> required;
 	std::vector<std::string> optional;
-
-
 };
 
+void usage() {
+	cout << endl;
+	cout << "Run as" << endl;
+	cout << "   % " << programName << " --configfile <config.txt>" << endl;
+	cout << "For help" << endl;
+	cout << "   % " << programName << " --help" << endl;
+	cout << endl;
+}
 
-// Helper function to clean up main.
-Options setupOptions(int theArgc, char * theArgv[]);
+void version() {
+	cout << endl;
+	cout << "Program " << programName << " v." << programVersion << ", " << programDate << ", (MSL v." << mslVersion << " " << mslDate << ")" << endl;
+}
+
+void help() {
+	cout << "Run as: " << endl;
+	cout << " % runQuench \n --pdb <PDB> " << endl;
+	cout << endl;
+	cout << "Optional Parameters " << endl;
+	cout << " --topfile <TOPFILE> " << endl;
+	cout << " --parfile <PARFILE> " << endl;
+	cout << " --rotlib <ROTLIB> " << endl;
+	cout << " --outfile <OUTPDB> " << endl;
+	cout << " --positions <CHAIN_POS> " << endl;
+	cout << " --rotlevel <SL85.00> " << endl;
+	cout << " --smallRotNum <NUM> " << endl;
+	cout << " --largeRotNum <NUM> " << endl;
+	cout << " --autoFinPositions" << endl;
+	cout << endl;
+}
+
+Options setupOptions(int theArgc, char * theArgv[]){
+	/******************************************
+	 *  Pass the array of argument and the name of
+	 *  a configuration file to the ArgumentParser
+	 *  object.  Then ask for the value of the argument
+	 *  and collect error and warnings.
+	 *
+	 *  This function returns a Options structure
+	 *  defined at the head of this file 
+	 ******************************************/
+	Options opt;
+	
+	/******************************************
+	 *  Set the allowed and required options:
+	 ******************************************/
+	// Required arguments
+	opt.required.push_back("pdb");
+
+	// Optional arguments
+	opt.optional.push_back("topfile");
+	opt.optional.push_back("parfile");
+	opt.optional.push_back("rotlib");
+	opt.optional.push_back("outfile");
+	opt.optional.push_back("positions");
+	opt.optional.push_back("largeRotNum");
+	opt.optional.push_back("smallRotNum");
+	opt.optional.push_back("autoFindPositions");
+	opt.optional.push_back("rotlevel"); 
+
+	OptionParser OP;
+	OP.readArgv(theArgc, theArgv);
+	OP.setRequired(opt.required);	
+	OP.setAllowed(opt.optional);
+
+	if (OP.countOptions() == 0) {
+		usage();
+		exit(0);
+	}
+	/*****************************************
+	 *  VERSION AND HELP
+	 *
+	 *  --version prints the version number
+	 *  --help prints the usage and help
+	 *****************************************/
+	opt.version = OP.getBool("version");
+
+	if (opt.version) {
+		version();
+		exit(0);
+	}
+
+	opt.help = OP.getBool("help");
+
+	if (opt.help) {
+		help();
+		exit(0);
+	}
+
+	/*****************************************
+	 *  CHECK THE GIVEN OPTIONS
+	 *****************************************/
+	opt.errorFlag = false;
+	opt.warningFlag = false;
+	opt.errorMessages = "";
+	opt.warningMessages = "";
+
+	opt.configfile = OP.getString("configfile");
+	
+	if (opt.configfile != "") {
+		OP.readFile(opt.configfile);
+		if (OP.fail()) {
+			opt.errorFlag = true;
+			opt.errorMessages += "Cannot read configuration file " + opt.configfile + "\n";
+			return opt;
+		}
+	}
+	if (!OP.checkOptions()) {
+		opt.errorFlag = true;
+		opt.OPerrors = OP.getErrors();
+		return opt;
+	}
+
+	opt.pdb  = OP.getString("pdb");
+	if (OP.fail()){
+		opt.errorMessages = "pdb not specified\n";
+		opt.errorFlag = true;
+		return opt;
+	}
+
+	opt.topfile = OP.getString("topfile");
+	if (OP.fail()){
+		opt.topfile = SYSENV.getEnv("MSL_CHARMM_TOP");
+		opt.warningMessages += "charmmtopfile not specified, using " + opt.topfile + "\n";
+		opt.warningFlag = true;
+	}
+
+	opt.parfile = OP.getString("parfile");
+	if (OP.fail()){
+		opt.parfile = SYSENV.getEnv("MSL_CHARMM_PAR");
+		opt.warningMessages += "charmmparfile not specified, using " + opt.parfile + "\n";
+		opt.warningFlag = true;
+	}
+
+	opt.rotlib = OP.getString("rotlib");
+	if (OP.fail()){
+		opt.rotlib = SYSENV.getEnv("MSL_ROTLIB");
+		opt.warningMessages += "rotlib not specified, using " + opt.rotlib + "\n";
+		opt.warningFlag = true;
+	}
+
+	opt.outfile = OP.getString("outfile");
+	if (OP.fail()){
+		opt.outfile = "currentConformation.pdb";
+		opt.warningMessages += "outfile not specified, using " + opt.outfile + "\n";
+		opt.warningFlag = true;
+	}
+
+	opt.positions = OP.getStringVectorJoinAll("positions");
+
+	opt.rotLevel = OP.getString("rotlevel");
+	if(OP.fail()) {
+		opt.rotLevel = "";
+	}
+
+	opt.largeRotNum = OP.getInt("largeRotNum");
+	if ((opt.rotLevel == "") && OP.fail()){
+		opt.warningMessages += "largeRotNum not specified, using 50\n";
+		opt.largeRotNum = 50;
+		opt.warningFlag = true;
+	}
+
+	opt.smallRotNum = OP.getInt("smallRotNum");
+	if ((opt.rotLevel == "") && OP.fail()){
+		opt.warningMessages += "smallRotNum not specified, using 5\n";
+		opt.smallRotNum = 5;
+		opt.warningFlag = true;
+	}
+	if (opt.largeRotNum < 0 || opt.smallRotNum < 0) {
+		opt.errorFlag = true;
+		opt.errorMessages += "Need a positive rotamer number\n";
+		return opt;
+	}
+
+	opt.autoFindPositions = OP.getBool("autoFindPositions");
+	if (OP.fail()){
+		opt.autoFindPositions = false;
+	}
+
+	return opt;
+}
