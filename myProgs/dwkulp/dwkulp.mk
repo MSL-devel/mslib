d1fff80764f702f3bc2649602be149f186281d45
Dan Kulp <dwkulp@gmail.com>
2012-12-12 23:43:46 +0000

    added metalRotamers patchAnalysis compareRosettaModels setupRosettaFixbb getConditionalPhiPsi refinePotentialFusions createTertFragDB buildRotamers getLoopLengths

--- myProgs/dwkulp/dwkulp.mk
+++ myProgs/dwkulp/dwkulp.mk
@@ -7,8 +7,10 @@ MYPROGS   = discoverMotif generateRotamerLibrary getTripletCaMeasurements superR
             buildInverseRotamers findCisPepBonds createVectorHash splitIntoChains getAAStats searchForFusions generateBioUnit \
             findPositionsWithRotamers dualLoopSearch insertSelectionIntoTemplate buildBinaryDMs multiSearchDM compareStructures \
 	    querySeqCons calcSasaAll designLinearSegment checkRMS_EOD conformationalSampling glycineSearch clashCheck setupRemodelFusions \
-	    headless_resfiles calcSasaInterface circularPermutant clusterDiagnols checkCloseTerminii getVectorPairs findPositionsWithRotamersRon \
-	    setupRosettaMSA designCheck domainSasa getMeasurement getPairEnergy
+	    headless_resfiles circularPermutant clusterDiagnols checkCloseTerminii getVectorPairs findPositionsWithRotamersRon \
+	    setupRosettaMSA designCheck domainSasa getMeasurement getPairEnergy metalRotamers patchAnalysis compareRosettaModels setupRosettaFixbb \
+	    getConditionalPhiPsi refinePotentialFusions createTertFragDB buildRotamers getLoopLengths
+#calcSasaInterface
 
 MYHEADERS = 
 
