4effc5d95423fb5e820a51926861bd6c2259fc3f
Dan Kulp <dwkulp@gmail.com>
2013-02-05 21:29:07 +0000

    added PDBFragment searching

--- myProgs/dwkulp/refinePotentialFusions.cpp
+++ myProgs/dwkulp/refinePotentialFusions.cpp
@@ -9,14 +9,20 @@
 #include "System.h"
 #include "Chain.h"
 #include "Residue.h"
+#include "MslOut.h"
+#include "PDBFragments.h"
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <queue>
+
 using namespace std;
 using namespace MSL;
 using namespace MslTools;
+
+static MslOut MSLOUT("refinePotentialFusions");
 
 int numberOfMainChainClashes(AtomPointerVector &_partner, AtomPointerVector  &_ref, int _clashTolerance=-1, int _removePartnerRes1=0, int _removeParterRes2=0,int _removeRefRes1=0,int _removeRefRes2=0);
 
@@ -68,6 +74,8 @@ int main(int argc, char *argv[]) {
     // For a range of Z-values 	 (+/- 5 Angstroms)
     CartesianPoint z_coord(0.0,0.0,1.0);
 
+    std::priority_queue< std::pair<int,string>, std::vector< std::pair<int,string> > > lowClashModels;
+
     // Sample DOFs
     scaffoldChains.saveCoor("pre");
     for (double transZ = -opt.transZ; transZ < opt.transZ; transZ += 1.0){
@@ -103,11 +111,25 @@ int main(int argc, char *argv[]) {
 	// Clash Check between scaffoldChains and notScaffoldChains
 	int numClashes = numberOfMainChainClashes(scaffoldChains,notScaffoldChains,minClashes,pXYZ_1.getResidueNumber(),pXYZ_2.getResidueNumber());
 	//cout << "NUM CLASHES: "<<numClashes<<" "<<minClashes<<endl;
-	if (numClashes < minClashes){
-	  fprintf(stdout,"New conformation: %3d clashes %8.3f transZ %8.3f rotZ [ %8.3f , %8.3f ]\n",numClashes,transZ,rotZ,sqrt(D1),sqrt(D2));
+	if (lowClashModels.size() < opt.numLowClashModels || numClashes < lowClashModels.top().first){
+
+	  fprintf(stdout,"New conformation: %3d clashes %8.3f transZ %8.3f rotZ [ %8.3f , %8.3f ]",numClashes,transZ,rotZ,sqrt(D1),sqrt(D2));
+
+	  if (numClashes < minClashes){
+	    minClashes = numClashes;
+	    scaffoldChains.saveCoor("minClashes");
+	  }
+
+	  if (lowClashModels.size() == opt.numLowClashModels){
+	    fprintf(stdout, " -- previous top -- %s %d", lowClashModels.top().second.c_str(),lowClashModels.top().first);
+	    scaffoldChains.clearSavedCoor(lowClashModels.top().second);
+	    lowClashModels.pop();
+	  } 
+	  fprintf(stdout,"\n");
 	  fflush(stdout);
-	  minClashes = numClashes;
-	  scaffoldChains.saveCoor("minClashes");
+	  string modelLabel = MslTools::stringf("%8.2f:%8.2f",transZ,rotZ);
+	  scaffoldChains.saveCoor(modelLabel);
+	  lowClashModels.push(pair<int,string>(numClashes, modelLabel));
 	}
 
 	scaffoldChains.applySavedCoor("post_transZ");	
@@ -119,11 +141,106 @@ int main(int argc, char *argv[]) {
       cerr << "ERROR 9999 No conformations found within junction distance\n";
       exit(9999);
     }
+    /*
     scaffoldChains.applySavedCoor("minClashes");
 
     string fname = MslTools::stringf("minClashes_%s.pdb", MslTools::getFileName(opt.pdb).c_str());
     pdb.writePdb(fname);
     fprintf(stdout, "%-50s %6d\n",fname.c_str(),minClashes);
+    */
+
+    // Load PDB Fragment dataset
+    MSLOUT.stream() << "Loading pdb fragment database..."<<endl;
+    PDBFragments frag(opt.fragdb);
+    frag.setPdbDir(opt.pdbdir);
+    frag.loadFragmentDatabase();
+
+    vector<string> stem1positions;
+    vector<string> stem2positions;
+    /*
+    stem1positions.push_back(opt.junction_positionXYZ_1);
+    stem1positions.push_back(opt.junction_positionXYZ_2);
+    stem2positions.push_back(opt.junction_positionABC_1);
+    stem2positions.push_back(opt.junction_positionABC_2);
+    */
+
+    
+    int xyz_1_index = pdb.getPositionIndex(&pXYZ_1);
+    int xyz_2_index = pdb.getPositionIndex(&pXYZ_2);
+    int abc_1_index = pdb.getPositionIndex(&pABC_1);
+    int abc_2_index = pdb.getPositionIndex(&pABC_2);
+	
+    int modelNum = 1;
+    while (!lowClashModels.empty()){
+      scaffoldChains.applySavedCoor(lowClashModels.top().second);
+      lowClashModels.pop();
+     
+
+      MSLOUT.stream() << "Searching pdb fragment database... for model "<<modelNum<<endl;
+      string fname = MslTools::stringf("lowClashes_%06d.pdb",modelNum);
+      pdb.writePdb(fname);
+      fprintf(stdout, "%-50s\n",fname.c_str());
+
+
+      // +/-2 positions
+      for (int p1 = -2; p1 <= 2;p1++){
+	string pos1 = MslTools::getPositionId(pdb.getPosition(xyz_1_index).getChainId(),pdb.getPosition(xyz_1_index).getResidueNumber()+p1,"");
+	if (! pdb.positionExists(pos1)) continue;
+	if ( pdb.getPosition(pos1).getChainId() != pXYZ_1.getChainId()) continue;
+
+	for (int p2 = -2; p2 <= 2;p2++){
+
+	  string pos2 = MslTools::getPositionId(pdb.getPosition(xyz_2_index).getChainId(),pdb.getPosition(xyz_2_index).getResidueNumber()+p2,"");
+	  if (! pdb.positionExists(pos2)) continue;
+	  if ( pdb.getPosition(pos2).getChainId() != pXYZ_2.getChainId()) continue;
+
+	  for (int p3 = -2; p3 <= 2;p3++){
+
+	    string pos3 = MslTools::getPositionId(pdb.getPosition(abc_1_index).getChainId(),pdb.getPosition(abc_1_index).getResidueNumber()+p3,"");
+	    if (! pdb.positionExists(pos3)) continue;
+	    if ( pdb.getPosition(pos3).getChainId() != pABC_1.getChainId()) continue;
+
+	    for (int p4 = -2; p4 <= 2;p4++){
+
+	      string pos4 = MslTools::getPositionId(pdb.getPosition(abc_2_index).getChainId(),pdb.getPosition(abc_2_index).getResidueNumber()+p4,"");
+	      if (! pdb.positionExists(pos4)) continue;
+	      if ( pdb.getPosition(pos4).getChainId() != pABC_2.getChainId()) continue;
+
+	      MSLOUT.stream() << "Search using stems: "<<pos1<<" "<<pos2<<" "<<pos3<<" "<<pos4<<endl;
+	      stem1positions.clear();
+	      stem1positions.push_back(pos1);
+	      stem1positions.push_back(pos2);
+
+	      stem2positions.clear();
+	      stem2positions.push_back(pos3);
+	      stem2positions.push_back(pos4);
+	      
+	      int numHits = 0;
+	      try {
+		numHits = frag.searchForMatchingDualFragments(pdb,stem1positions,
+							      pdb,stem2positions,
+							      opt.loop1min,opt.loop1max,opt.loop2min,opt.loop2max,opt.distanceStem1,opt.distanceStem2, opt.stemRmsdTol,opt.totalRmsdTol,false);
+	      } catch (std::exception &e){
+		MSLOUT.stream() << e.what()<<endl;
+	      }
+	      MSLOUT.fprintf(stdout, "\thits = %10d\n",numHits);
+
+	      vector<AtomContainer *> &dualLoops = frag.getAtomContainers();
+	      for (uint h = 0; h < dualLoops.size();h++){
+		string loopname=MslTools::stringf("%s_%05d_%d_%d_%d_%d.pdb",MslTools::getFileName(fname).c_str(),h+1,p1,p2,p3,p4);
+		dualLoops[h]->writePdb(loopname);
+	      }
+	    } // FOR p4
+	  } // FOR p3
+	} // FOR p2
+      } // FOR p1
+
+
+
+
+
+      modelNum++;
+    }
 }
 
 Options setupOptions(int theArgc, char * theArgv[]){
@@ -204,6 +321,61 @@ Options setupOptions(int theArgc, char * theArgv[]){
       opt.rotZ = 10;
       cerr << "WARNING default rotZ to "<<opt.rotZ<<endl;
     }    
+
+    opt.numLowClashModels = OP.getInt("numLowClashModels");
+    if (OP.fail()){
+      opt.numLowClashModels = 1;
+    }
+
+    
+
+	opt.distanceStem1 = OP.getDouble("distanceStem1");
+	if (OP.fail()){
+		cerr << "WARNING 1111 no distanceStem1 specified."<<endl;
+		opt.distanceStem1 = 0.0;
+	}
+
+	opt.distanceStem2 = OP.getDouble("distanceStem2");
+	if (OP.fail()){
+		cerr << "WARNING 1111 no distanceStem2 specified."<<endl;
+		opt.distanceStem2 = 0.0;
+	}
+
+
+	opt.loop1min = OP.getInt("loop1min");
+	if (OP.fail()){
+		cerr << "ERROR 1111 no loop1min specified."<<endl;
+		exit(1111);
+	}
+
+	opt.loop1max = OP.getInt("loop1max");
+	if (OP.fail()){
+		cerr << "ERROR 1111 no loop1max specified."<<endl;
+		exit(1111);
+	}
+
+	opt.loop2min = OP.getInt("loop2min");
+	if (OP.fail()){
+		cerr << "ERROR 1111 no loop2min specified."<<endl;
+		exit(1111);
+	}
+
+	opt.loop2max = OP.getInt("loop2max");
+	if (OP.fail()){
+		cerr << "ERROR 1111 no loop2max specified."<<endl;
+		exit(1111);
+	}
+
+	opt.stemRmsdTol = OP.getDouble("stemRmsdTol");
+	if (OP.fail()){
+	  opt.stemRmsdTol = 0.3;
+	}
+	opt.totalRmsdTol = OP.getDouble("totalRmsdTol");
+	if (OP.fail()){
+	  opt.totalRmsdTol = 0.3;
+	}
+	opt.fragdb = OP.getString("fragDB");
+	opt.pdbdir = OP.getString("pdbdir");
 
     cout << OP<<endl;
     return opt;
--- myProgs/dwkulp/refinePotentialFusions.h
+++ myProgs/dwkulp/refinePotentialFusions.h
@@ -16,6 +16,21 @@ struct Options {
 		required.push_back("max_junction_distance");
 		required.push_back("transZ_limit");
 		required.push_back("rotZ_limit");
+		optional.push_back("numLowClashModels");
+
+
+		required.push_back("distanceStem1");
+		required.push_back("distanceStem2");
+
+		required.push_back("loop1min");
+		required.push_back("loop1max");
+		required.push_back("loop2min");
+		required.push_back("loop2max");
+
+		optional.push_back("stemRmsdTol");
+		optional.push_back("totalRmsdTol");
+		optional.push_back("fragDB");
+		optional.push_back("pdbdir");
 	}
 
 
@@ -29,6 +44,18 @@ struct Options {
 	double max_junction_distance;
         double transZ;
         double rotZ;
+        int numLowClashModels;
+        string fragdb;
+        string pdbdir;
+        double distanceStem1;
+        double distanceStem2;
+        int loop1min;
+        int loop1max;
+        int loop2min;
+        int loop2max;
+
+        double stemRmsdTol;
+        double totalRmsdTol;
 	vector<string> required;
 	vector<string> optional;
 	vector<string> defaultArgs;
