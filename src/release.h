bb8ce15bb128d57b4489ece5457b6ab8914972f0
Jason Donald <jedonald@gmail.com>
2013-03-22 18:33:03 +0000

    Rolling release file.

--- src/release.h
+++ src/release.h
@@ -30,11 +30,13 @@ You should have received a copy of the GNU Lesser General Public
 #ifndef RELEASE_H
 #define RELEASE_H
 
-#define MSLVERSION "1.1.1.3"
+#define MSLVERSION "1.1.1.4"
 #define MSLDATE "March 22, 2013"
 
 /*
 HISTORY:
+1.1.1.4    March 22, 2013    jedonald
+                'src/Quench.cpp' -Fix bug related to shuffling repacking order (second change)
 1.1.1.3    March 22, 2013    jedonald
                 'src/Position.cpp' -Check for hiddenIdentityIndeces vector values, otherwise will segfault
                 'src/RandomNumberGenerator.h', 'src/RandomNumberGenerator.cpp' -Allow for random integers to not include upper
