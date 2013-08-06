beff290b94baf8583839ea7a6b6847884beab2af
Jason Donald <jedonald@gmail.com>
2013-08-06 20:07:16 +0000

    Modify regular expression searches to match more MSA output formats

--- src/ALNReader.cpp
+++ src/ALNReader.cpp
@@ -55,71 +55,52 @@ bool ALNReader::read(){
 	}
 
 	try { 
-	       string headerExp = "^CLUSTAL W (\\S+)\\s+([\\S\\s]+)$";
-	       string dataExp   = "^(\\S+)\\s+(\\S+)\\s([0-9]+)$";
-	       string equivExp  = "^([\\s\\S]+)$";
-	       int seqIndex = 0;
+		string headerExp = " \\(*([0-9\\.])\\)*\\s+([\\S\\s]+)$";
+		string dataExp   = "^(\\S+)\\s+(\\S+)\\s([0-9]+)$";
+		string equivExp  = "^([\\s\\S]+)$";
+		int seqIndex = 0;
+		int lineIndex = 0;
 		while (!endOfFileTest()){
 			string line = Reader::getLine();
 
 			// Skip blank lines.
-			if (line.size() == 0) {
-			  continue;
+			if (line.size() != 0) {
+				// Storage for regular expression matches for this line
+				vector<string> reTokens;
+				
+				// Need to parse header line (CLUSTAL W 2.1 ...)
+				if ((lineIndex == 0) && MslTools::regex(line,headerExp,reTokens)){
+					version = reTokens[0];
+					remark  = reTokens[1];
+					lineIndex++;
+				} else if (MslTools::regex(line,dataExp,reTokens)){
+					// Parse data lines
+					map<string,string>::iterator it;
+					it = sequences.find(reTokens[0]);
+					if (it == sequences.end()){
+					  sequences[reTokens[0]] = reTokens[1];
+					} else {
+					  sequences[reTokens[0]] += reTokens[1];
+					}
+
+					// Find index of first character not key+spaces
+					seqIndex = line.find(reTokens[1]);
+				} else if (MslTools::regex(line.substr(seqIndex),equivExp,reTokens)){
+					// Parse Residue equivalency lines
+					map<string,string>::iterator it;
+					it = sequences.find("equivRes");
+					if (it == sequences.end()){
+					  sequences["equivRes"] = reTokens[0];
+					} else {
+					  sequences["equivRes"] += reTokens[0];
+					}
+				}
 			}
-
-			// Storage for regular expression matches for this line
-			vector<string> reTokens;
-			
-			// Need to parse header line (CLUSTAL W 2.1 ...)
-			if (MslTools::regex(line,headerExp,reTokens)){
-			  
-			  version = reTokens[0];
-			  remark  = reTokens[1];
-			  continue;
-			}
-			
-			// Parse data lines
-			if (MslTools::regex(line,dataExp,reTokens)){
-
-			  map<string,string>::iterator it;
-			  it = sequences.find(reTokens[0]);
-			  if (it == sequences.end()){
-			    sequences[reTokens[0]] = reTokens[1];
-			  } else {
-			    sequences[reTokens[0]] += reTokens[1];
-			  }
-
-			  // Find index of first character not key+spaces
-			  seqIndex = line.find(reTokens[1]);
-			  
-			  
-			  continue;
-			}
-
-			// Parse Residue equivalency lines
-			if (MslTools::regex(line.substr(seqIndex),equivExp,reTokens)){
-			  
-			  map<string,string>::iterator it;
-			  it = sequences.find("equivRes");
-			  if (it == sequences.end()){
-			    sequences["equivRes"] = reTokens[0];
-			  } else {
-			    sequences["equivRes"] += reTokens[0];
-			  }
-
-			  continue;
-			}
-
-			
-
-
-
 		}
 	} catch(...){
 	  cerr << "ERROR 9090 in ALNReader::read()\n";
 	  return false;
 	}
-
 
 	return true;
 }
