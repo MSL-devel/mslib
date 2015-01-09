d4b51d3415fe8b51125df1d2480bb24e2f94e124
Jason Donald <jedonald@gmail.com>
2015-01-09 22:07:37 +0000

    Rolling release file.

--- src/release.h
+++ src/release.h
@@ -30,11 +30,14 @@ You should have received a copy of the GNU Lesser General Public
 #ifndef RELEASE_H
 #define RELEASE_H
 
-#define MSLVERSION "1.2.3.1"
-#define MSLDATE "December 20, 2014"
+#define MSLVERSION "1.2.3.2"
+#define MSLDATE "January 09, 2015"
 
 /*
 HISTORY:
+1.2.3.2    January 09, 2015    jedonald
+                'myProgs/jedonald/jedonald.mk' -Update personal makefile
+                'myProgs/jedonald/findSaltBridges.cpp' -Short program to print out salt bridge residue numbers
 1.2.3.1    December 20, 2014    sabareeshs
                WARNING!  Files submitted without building tree or running tests.
                 'programs/repackSideChains.cpp', 'programs/repackSideChains.h', 'src/RotamerLibrary.cpp', 'src/SystemRotamerLoader.cpp'
